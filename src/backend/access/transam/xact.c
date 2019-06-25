e84ce3675eaae41af5427221dce372ac3e2382be
David Kimura <dkimura@pivotal.io>
2019-06-25 18:13:10 +0000

    Remove XLOG_XACT_ONE_PHASE_COMMIT xlog record
    Issue is that in Postgres 9.5 there is a fixed amount of xl_info flags
    that can be used. GPDB commit b587100923e2 added another flag which in
    GPDB 9.5 merge branch exceeds the amount available.
    
    Ashwin noticed the difference between XLOG_XACT_ONE_PHASE_COMMIT and
    XLOG_XACT_COMMIT records is if distributed info is set. Instead we can
    use a same record type in both cases if we setup distributed info in
    RecordTransactionCommit() and then pass the distributed info through
    xact_redo().
    
    (cherry picked from commit 4be677c7feb05a3dfcea9420b4ebeab4c0de6d4e)

--- src/backend/access/transam/xact.c
+++ src/backend/access/transam/xact.c
@@ -1287,8 +1287,8 @@ RecordTransactionCommit(void)
 	bool		isDtxPrepared = 0;
 	TMGXACT_LOG gxact_log;
 	XLogRecPtr	recptr = InvalidXLogRecPtr;
-	DistributedTransactionTimeStamp distribTimeStamp;
-	DistributedTransactionId distribXid;
+	DistributedTransactionTimeStamp distribTimeStamp = 0;
+	DistributedTransactionId distribXid = 0;
 
 	/* Like in CommitTransaction(), treat a QE reader as if there was no XID */
 	if (DistributedTransactionContext == DTX_CONTEXT_QE_ENTRY_DB_SINGLETON ||
@@ -1487,14 +1487,10 @@ RecordTransactionCommit(void)
 
 				insertedDistributedCommitted();
 			}
-			else if (Gp_role == GP_ROLE_EXECUTE && MyTmGxact->isOnePhaseCommit)
+			else
 			{
 				xlrec.distribTimeStamp = distribTimeStamp;
 				xlrec.distribXid = distribXid;
-				recptr = XLogInsert(RM_XACT_ID, XLOG_XACT_ONE_PHASE_COMMIT, rdata);
-			}
-			else
-			{
 				recptr = XLogInsert(RM_XACT_ID, XLOG_XACT_COMMIT, rdata);
 			}
 
@@ -6415,7 +6411,7 @@ xact_redo(XLogRecPtr beginLoc __attribute__((unused)), XLogRecPtr lsn __attribut
 	{
 		xl_xact_commit *xlrec = (xl_xact_commit *) XLogRecGetData(record);
 
-		xact_redo_commit(xlrec, record->xl_xid, lsn, 0, 0);
+		xact_redo_commit(xlrec, record->xl_xid, lsn, xlrec->distribXid, xlrec->distribTimeStamp);
 	}
 	else if (info == XLOG_XACT_ABORT)
 	{
@@ -6462,12 +6458,6 @@ xact_redo(XLogRecPtr beginLoc __attribute__((unused)), XLogRecPtr lsn __attribut
 		if (standbyState >= STANDBY_INITIALIZED)
 			ProcArrayApplyXidAssignment(xlrec->xtop,
 										xlrec->nsubxacts, xlrec->xsub);
-	}
-	else if (info == XLOG_XACT_ONE_PHASE_COMMIT)
-	{
-		xl_xact_commit *xlrec = (xl_xact_commit *) XLogRecGetData(record);
-
-		xact_redo_commit(xlrec, record->xl_xid, lsn, xlrec->distribTimeStamp, xlrec->distribXid);
 	}
 	else
 		elog(PANIC, "xact_redo: unknown op code %u", info);
