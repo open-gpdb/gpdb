8ef3b437d554fdedffc375981e3e1dc115798302
Heikki Linnakangas <heikki.linnakangas@iki.fi>
2019-07-30 21:14:14 +0300

    Print WAL position correctly in pg_rewind error message.
    This has been wrong ever since pg_rewind was added. The if-branch just
    above this, where we print the same error with an extra message supplied
    by XLogReadRecord() got this right, but the variable name was wrong in the
    else-branch. As a consequence, the error printed the WAL position as
    0/0 if there was an error reading a WAL file.
    
    Backpatch to 9.5, where pg_rewind was added.

--- src/bin/pg_rewind/parsexlog.c
+++ src/bin/pg_rewind/parsexlog.c
@@ -96,9 +96,8 @@ extractPageMap(const char *datadir, XLogRecPtr startpoint, TimeLineID tli,
 						 (uint32) (errptr >> 32), (uint32) (errptr),
 						 errormsg);
 			else
-				pg_fatal("could not read WAL record at %X/%X\n",
-						 (uint32) (startpoint >> 32),
-						 (uint32) (startpoint));
+				pg_fatal("could not read WAL record at %X/%X",
+						 (uint32) (errptr >> 32), (uint32) (errptr));
 		}
 
 		extractPageInfo(record);
