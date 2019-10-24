ae44e671113af437e239b4f1772caaee5928cb82
Soumyadeep Chakraborty <sochakraborty@pivotal.io>
2019-10-24 10:49:16 -0700

    6X Backport: Elevate force_mirrors_to_catch_up() to gp_inject_fault
    This backports 5feccaae.
    
    This also applies the fix in efd76c4c4 to all callers of
    force_mirrors_to_catch_up(), thus eliminating flakes such as:
    
    ```
    -- Ensure that the mirrors including the standby master have removed the dboid dir under the target tablespace.
     SELECT gp_wait_until_triggered_fault('after_drop_database_directories', 1, dbid) FROM gp_segment_configuration WHERE role='m';
    - gp_wait_until_triggered_fault
    --------------------------------
    - Success:
    - Success:
    - Success:
    - Success:
    -(4 rows)
    -
    +ERROR:  failed to inject fault: ERROR:  fault not triggered, fault name:'after_drop_database_directories' fault type:'wait_until_triggered' (gp_inject_fault.c:127)
    +DETAIL:  DETAIL:  Timed-out as 10 minutes max wait happens until triggered.
    ```

--- gpcontrib/gp_inject_fault/gp_inject_fault.c
+++ gpcontrib/gp_inject_fault/gp_inject_fault.c
@@ -1,24 +1,23 @@
 #include "postgres.h"
 
 #include "funcapi.h"
-#include "miscadmin.h"
 
 #include "access/heapam.h"
 #include "access/genam.h"
+#include "access/xlog.h"
 #include "catalog/indexing.h"
 #include "cdb/cdbdisp_query.h"
 #include "cdb/cdbvars.h"
 #include "libpq/ip.h"
 #include "libpq-fe.h"
-#include "postmaster/postmaster.h"
 #include "utils/builtins.h"
 #include "utils/faultinjector.h"
 #include "utils/fmgroids.h"
-#include "utils/snapmgr.h"
 
 PG_MODULE_MAGIC;
 
 extern Datum gp_inject_fault(PG_FUNCTION_ARGS);
+Datum insert_noop_xlog_record(PG_FUNCTION_ARGS);
 
 static void
 getHostnameAndPort(int dbid, char **hostname, int *port)
@@ -140,4 +139,25 @@ gp_inject_fault(PG_FUNCTION_ARGS)
 			elog(ERROR, "%s", response);
 	}
 	PG_RETURN_TEXT_P(cstring_to_text(response));
+}
+
+PG_FUNCTION_INFO_V1(insert_noop_xlog_record);
+Datum
+insert_noop_xlog_record(PG_FUNCTION_ARGS)
+{
+	char *no_op_string = "no-op";
+	
+	XLogRecData rdata = {};
+	/* 
+	 * Xlog records of length = 0 are disallowed and cause a panic. Thus,
+	 * supplying a dummy non-zero length
+	 */
+	rdata.data = no_op_string;
+	rdata.len = strlen(no_op_string);
+	rdata.buffer = InvalidBuffer;
+	rdata.next = NULL;
+
+	XLogFlush(XLogInsert(RM_XLOG_ID, XLOG_NOOP, &rdata));
+
+	PG_RETURN_VOID();
 }
