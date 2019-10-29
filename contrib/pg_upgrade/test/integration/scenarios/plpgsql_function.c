f871cbe515e89fc680583a2da87cddb96f51d9e8
Georgios Kokolatos <gkokolatos@pivotal.io>
2019-10-29 11:27:38 +0100

    Add a plpgsql test case for upgrade from 5X to 6X
    This is the most basic case scenario where a simple immutable function can be
    successfully upgraded. More elaborate cases of plpgsql syntax and/or usage are
    not needed to be tested as the same catalog entries are used.
    
    Co-authored-by: Adam Berlin <aberlin@pivotal.io>
    Reviewed-by: David Kimura <dkimura@pivotal.io>

---
+++ contrib/pg_upgrade/test/integration/scenarios/plpgsql_function.c
@@ -0,0 +1,78 @@
+#include <setjmp.h>
+#include <stdarg.h>
+#include <stddef.h>
+#include <stdlib.h>
+
+#include "cmockery.h"
+#include "libpq-fe.h"
+
+#include "utilities/bdd-helpers.h"
+#include "utilities/gpdb5-cluster.h"
+#include "utilities/gpdb6-cluster.h"
+#include "utilities/query-helpers.h"
+#include "utilities/test-helpers.h"
+#include "utilities/upgrade-helpers.h"
+
+#include "plpgsql_function.h"
+
+static void
+createPlpgsqlFunctionInFiveCluster(void)
+{
+	PGconn	   *conn1 = connectToFive();
+	PGresult   *result;
+
+	result = executeQuery(conn1, "CREATE SCHEMA five_to_six_upgrade;");
+	PQclear(result);
+
+	result = executeQuery(conn1, "SET search_path TO five_to_six_upgrade;");
+	PQclear(result);
+
+	result = executeQuery(conn1, "                                  \
+		CREATE FUNCTION someimmutablefunction(foo integer)          \
+		RETURNS integer                                             \
+		LANGUAGE plpgsql IMMUTABLE STRICT AS                        \
+		$$                                                          \
+		BEGIN                                                       \
+			return 42 + foo;                                        \
+		END                                                         \
+		$$;                                                         \
+	");
+	PQclear(result);
+
+	PQfinish(conn1);
+}
+
+static void
+anAdministratorPerformsAnUpgrade(void)
+{
+	performUpgrade();
+}
+
+static void
+thePlpgsqlFunctionIsUsable(void)
+{
+	PGconn	   *connection = connectToSix();
+	PGresult   *result = NULL;
+	char	   *resvalue = NULL;
+	int			nrows;
+	int			actual;
+
+	result = executeQuery(connection, "SELECT five_to_six_upgrade.someimmutablefunction(0) as f;");
+	nrows = PQntuples(result);
+	resvalue = PQgetvalue(result, 0, PQfnumber(result, "f"));
+	actual = atoi((resvalue != NULL) ? resvalue : 0);
+
+	PQclear(result);
+	PQfinish(connection);
+
+	assert_int_equal(1, nrows);
+	assert_int_equal(42, actual);
+}
+
+void
+test_a_plpgsql_function_can_be_upgraded(void **state)
+{
+	given(withinGpdbFiveCluster(createPlpgsqlFunctionInFiveCluster));
+	when(anAdministratorPerformsAnUpgrade);
+	then(withinGpdbSixCluster(thePlpgsqlFunctionIsUsable));
+}
---
+++ contrib/pg_upgrade/test/integration/scenarios/plpgsql_function.h
@@ -0,0 +1,1 @@
+void test_a_plpgsql_function_can_be_upgraded(void **state);
