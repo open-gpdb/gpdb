2a95480307c775b3a7e8ad97a603d6830adb8074
David Kimura <dkimura@pivotal.io>
2019-10-30 00:20:58 +0000

    Fix upgrade integration test ports
    Commit 660a28433f9 updated port assignments for demo cluster. Update
    PORT_BASE in the upgrade integration framework accordingly to keep
    expected port assignment.
    
    Co-authored-by: Bhuvnesh Chaudhary <bhuvnesh2703@gmail.com>

--- contrib/pg_upgrade/test/integration/utilities/gpdb6-cluster.c
+++ contrib/pg_upgrade/test/integration/utilities/gpdb6-cluster.c
@@ -7,7 +7,7 @@ startGpdbSixCluster(void)
 {
 	system(""
 		   ". ./gpdb6/greenplum_path.sh; "
-		   "export PGPORT=60000; "
+		   ". ./configuration/gpdb6-env.sh; "
 		   "export MASTER_DATA_DIRECTORY=./gpdb6-data/qddir/demoDataDir-1; "
 		   "./gpdb6/bin/gpstart -a --skip_standby_check --no_standby"
 		);
@@ -18,7 +18,7 @@ stopGpdbSixCluster(void)
 {
 	system(""
 		   ". ./gpdb6/greenplum_path.sh; \n"
-		   "export PGPORT=60000; \n"
+		   ". ./configuration/gpdb6-env.sh; \n"
 		   "export MASTER_DATA_DIRECTORY=./gpdb6-data/qddir/demoDataDir-1; \n"
 		   "./gpdb6/bin/gpstop -a"
 		);
