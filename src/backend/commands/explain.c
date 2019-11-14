2448a442f5958aa1b5a47fbc46a325e5d0cb3a85
Ashuka Xue <axue@pivotal.io>
2019-11-13 16:23:27 -0800

    Update the explain for Dynamic Bitmap Index Scans
    Currently for plans generated by ORCA, the explain plan prints out the
    name of the first partitioned index, regardless of whether or not it is
    the correct index. This may be misleading because the executor actually
    looks at the entire index and properly selects only the indexes needed for
    the required partitions.
    
    The first partitioned index's OID is used to help the executor find the
    indexes needed. In order to prevent future misgivings, do not print the
    index name here in the explain plans for partition tables in ORCA.
    
    This does not affect explains by planner or other plans as they do not
    follow the same code path.
    
    Co-authored-by: Ashuka Xue <axue@pivotal.io>
    Co-authored-by: Chris Hajas <chajas@pivotal.io>

--- src/backend/commands/explain.c
+++ src/backend/commands/explain.c
@@ -17,6 +17,7 @@
 
 #include "access/xact.h"
 #include "catalog/pg_type.h"
+#include "cdb/cdbpartition.h"
 #include "commands/createas.h"
 #include "commands/defrem.h"
 #include "commands/prepare.h"
@@ -1631,11 +1632,30 @@ ExplainNode(PlanState *planstate, List *ancestors,
 			}
 			break;
 		case T_BitmapIndexScan:
-		case T_DynamicBitmapIndexScan:
 			{
 				BitmapIndexScan *bitmapindexscan = (BitmapIndexScan *) plan;
 				const char *indexname =
 				explain_get_index_name(bitmapindexscan->indexid);
+
+				if (es->format == EXPLAIN_FORMAT_TEXT)
+					appendStringInfo(es->str, " on %s", indexname);
+				else
+					ExplainPropertyText("Index Name", indexname, es);
+			}
+			break;
+		case T_DynamicBitmapIndexScan:
+			{
+				BitmapIndexScan *bitmapindexscan = (BitmapIndexScan *) plan;
+				Oid indexoid = bitmapindexscan->indexid;
+				Oid parentOid = rel_partition_get_root(indexoid);
+				while (parentOid != InvalidOid)
+				{
+					indexoid = parentOid;
+					parentOid = rel_partition_get_root(indexoid);
+				}
+
+				const char *indexname =
+				explain_get_index_name(indexoid);
 
 				if (es->format == EXPLAIN_FORMAT_TEXT)
 					appendStringInfo(es->str, " on %s", indexname);
