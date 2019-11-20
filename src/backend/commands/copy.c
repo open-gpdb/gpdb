0c04e42fba80b9fd33b40fae58ea84cdb46effc3
Ashwin Agrawal <aagrawal@pivotal.io>
2019-11-19 16:08:28 -0800

    Convey if QD encountered delim or not to QE
    This is necessary to differentiate between empty/missing next
    attribute from empty next attribute. QD encountering delim, means next
    attribute is empty and not really missing. QE needs to know this
    information, as it has no access to find this info, since the location
    QD ask QE to start reading is after delim if present or end of line.
    
    (cherry picked from commit b44459bcacd35bc6859d4193830afd76079800a8)

--- src/backend/commands/copy.c
+++ src/backend/commands/copy.c
@@ -348,6 +348,8 @@ typedef struct
 	uint32		line_len;			/* size of the included input line */
 	uint32		residual_off;		/* offset in the line, where QE should
 									 * process remaining fields */
+	bool		delim_seen_at_end;  /* conveys to QE if QD saw a delim at end
+									 * of its processing */
 	uint16		fld_count;			/* # of fields that were processed in the
 									 * QD. */
 
@@ -5089,7 +5091,8 @@ NextCopyFromX(CopyState cstate, ExprContext *econtext,
 			 * We have received the raw line from the QD, and we just
 			 * need to split it into raw fields.
 			 */
-			if (cstate->line_buf.cursor < cstate->line_buf.len)
+			if (cstate->stopped_processing_at_delim &&
+				cstate->line_buf.cursor <= cstate->line_buf.len)
 			{
 				if (cstate->csv_mode)
 					fldct = CopyReadAttributesCSV(cstate, -1);
@@ -5461,6 +5464,7 @@ retry:
 	cstate->line_buf_valid = true;
 	cstate->line_buf_converted = true;
 	cstate->cur_lineno = frame.lineno;
+	cstate->stopped_processing_at_delim = frame.delim_seen_at_end;
 
 	/*
 	 * Parse any fields from the input line that were not processed in the
@@ -5867,6 +5871,7 @@ SendCopyFromForwardedTuple(CopyState cstate,
 	frame->line_len = cstate->line_buf.len;
 	frame->residual_off = cstate->line_buf.cursor;
 	frame->fld_count = num_sent_fields;
+	frame->delim_seen_at_end = cstate->stopped_processing_at_delim;
 
 	if (toAll)
 		cdbCopySendDataToAll(cdbCopy, msgbuf->data, msgbuf->len);
@@ -6508,7 +6513,10 @@ CopyReadAttributesText(CopyState cstate, int stop_processing_at_field)
 		 * In QD, stop once we have processed the last field we need in the QD.
 		 */
 		if (fieldno == stop_processing_at_field)
+		{
+			cstate->stopped_processing_at_delim = true;
 			break;
+		}
 
 		/* Make sure there is enough space for the next value */
 		if (fieldno >= cstate->max_fields)
@@ -6672,7 +6680,10 @@ CopyReadAttributesText(CopyState cstate, int stop_processing_at_field)
 		fieldno++;
 		/* Done if we hit EOL instead of a delim */
 		if (!found_delim)
+		{
+			cstate->stopped_processing_at_delim = false;
 			break;
+		}
 	}
 
 	/*
@@ -6750,7 +6761,10 @@ CopyReadAttributesCSV(CopyState cstate, int stop_processing_at_field)
 		 * In QD, stop once we have processed the last field we need in the QD.
 		 */
 		if (fieldno == stop_processing_at_field)
+		{
+			cstate->stopped_processing_at_delim = true;
 			break;
+		}
 
 		/* Make sure there is enough space for the next value */
 		if (fieldno >= cstate->max_fields)
@@ -6855,7 +6869,10 @@ endfield:
 		fieldno++;
 		/* Done if we hit EOL instead of a delim */
 		if (!found_delim)
+		{
+			cstate->stopped_processing_at_delim = false;
 			break;
+		}
 	}
 
 	/*
