5dc6ffb6a375ec17a139118044d9b03808fd0a32
Jimmy Yih <jyih@pivotal.io>
2019-12-12 16:56:20 -0800

    Fix binary swap test for pg_get_viewdef() fix
    After pg_get_viewdef() was fixed to handle views defined with
    gp_dist_random(), the binary swap test started to fail because it had
    one of those views being incorrectly dumped. This is an expected
    failure and we'll need to remove the view before running the binary
    swap test.
    
    Expected failure diff:
    ```
    <    FROM ONLY public.locktest_segments_dist
    ---
    >    FROM gp_dist_random('public.locktest_segments_dist')
    ```
    
    GPDB Reference:
    https://github.com/greenplum-db/gpdb/commit/7af86a0bf2871a9e

---
+++ src/test/binary_swap/test_binary_swap_pre.sql
@@ -0,0 +1,12 @@
+-- WARNING: This file is executed against the postgres database. If
+-- objects are to be manipulated in other databases, make sure to
+-- change to the correct database first.
+
+-- The pg_get_viewdef() function was fixed to properly show views
+-- defined with gp_dist_random() but it will diff in this binary swap
+-- test suite. Ignore by removing the view.
+\c regression;
+DROP VIEW IF EXISTS locktest_segments;
+
+\c isolation2test;
+DROP VIEW IF EXISTS locktest_segments;
