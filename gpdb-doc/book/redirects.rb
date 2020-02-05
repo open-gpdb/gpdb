ae9dd076f900c857f27d7388c887ed673671c057
David Yozie <dyozie@pivotal.io>
2020-02-05 11:27:24 -0800

    Docs - change versioning form 6.3 -> 6.4

--- gpdb-doc/book/redirects.rb
+++ gpdb-doc/book/redirects.rb
@@ -1,4 +1,4 @@
-r301 '/', '/6-3/common/gpdb-features.html'
-r301 '/index.html', '/6-3/common/gpdb-features.html'
-r301 '/6-3/index.html', '/6-3/common/gpdb-features.html'
-r301 %r{(.*)/homenav.html}, '/6-3/common/gpdb-features.html'
+r301 '/', '/6-4/common/gpdb-features.html'
+r301 '/index.html', '/6-4/common/gpdb-features.html'
+r301 '/6-4/index.html', '/6-4/common/gpdb-features.html'
+r301 %r{(.*)/homenav.html}, '/6-4/common/gpdb-features.html'
