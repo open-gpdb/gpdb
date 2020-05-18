e0d07d8d0fc0c6cb5f93e8f2a85b1a5910626340
Ning Yu <nyu@pivotal.io>
2020-05-18 19:53:18 +0800

    ic-proxy: new value "proxy" in GUC gp_interconnect_type
    It is for the ic-proxy mode.
    
    (cherry picked from commit 245ca266088b4f8245cbcd6ded08aa165a195d49)

--- src/backend/utils/misc/guc_gp.c
+++ src/backend/utils/misc/guc_gp.c
@@ -535,6 +535,9 @@ static const struct config_enum_entry gp_interconnect_fc_methods[] = {
 static const struct config_enum_entry gp_interconnect_types[] = {
 	{"udpifc", INTERCONNECT_TYPE_UDPIFC},
 	{"tcp", INTERCONNECT_TYPE_TCP},
+#ifdef HAVE_LIBUV
+	{"proxy", INTERCONNECT_TYPE_PROXY},
+#endif  /* HAVE_LIBUV */
 	{NULL, 0}
 };
 
@@ -4773,7 +4776,11 @@ struct config_enum ConfigureNamesEnum_gp[] =
 	{
 		{"gp_interconnect_type", PGC_BACKEND, GP_ARRAY_TUNING,
 			gettext_noop("Sets the protocol used for inter-node communication."),
-			gettext_noop("Valid values are \"tcp\" and \"udpifc\".")
+			gettext_noop("Valid values are \"tcp\", \"udpifc\""
+#ifdef HAVE_LIBUV
+						 " and \"proxy\""
+#endif  /* HAVE_LIBUV */
+						 ".")
 		},
 		&Gp_interconnect_type,
 		INTERCONNECT_TYPE_UDPIFC, gp_interconnect_types,
