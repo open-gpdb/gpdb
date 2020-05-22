a797776fed862362831a6dd5196ac877eb840fda
Chris Hajas <chajas@pivotal.io>
2020-05-22 15:23:10 -0700

    Create explain pipeline for use with slack commands
    This creates an explain pipeline which is used to run query workloads
    using explain and identify plan differences during feature development.
    
    This previously existed on 5X, but with the Orca in GPDB effort the
    architecture needed to be updated.
    
    The new pipeline has the following features for use with slack commands:
    
    - Comparing a SHA with a baseline SHA
    - Identifying and outputting plan diffs
    - Summarizing the results with planning time, any
    fallbacks/crashes/timeouts, and the type of diff (cost, cardinality,
    plan change)
    - Enabling custom gucs (turning optimizer off, using a different cost
    model, enabling/disabling a feature flag/join order)
    
    Some use cases that we've come across:
    - Checking whether a new join order algorithm influences optimization
    time/dramatically changes plans
    
    One of the main goals with this change is to make it easy to add
    workloads. The slack command can be used in the following format:
    
    \test_gpdb6_explain <SHA_remote> <SHA> <baseline_SHA_remote>
    <baseline_SHA> <workload> [optional_gucs]
    
    where workloads is either "all" or a list with no spaces of the names of
    the workloads to run (eg: workload1,workload2,workload3).
    and gucs, if specified, are in the format:
    ```
    optimizer=off
    enable_nestloop=on
    ```

--- concourse/scripts/regression_tests_gpcloud.bash
+++ concourse/scripts/regression_tests_gpcloud.bash
@@ -39,12 +39,8 @@ function setup_gpadmin_user() {
   ./gpdb_src/concourse/scripts/setup_gpadmin_user.bash
 }
 
-function build_gpdb(){
-  ${SCRIPT_DIR}/build_gpdb.py --mode=planner --output_dir=${GPDB_INSTALL_DIR}
-}
-
 function make_cluster() {
-  PYTHONPATH=${SCRIPT_DIR}:${PYTHONPATH} python2 -c "from builds.GpBuild import GpBuild; GpBuild(\"planner\").create_demo_cluster()"
+  PYTHONPATH=${SCRIPT_DIR}:${PYTHONPATH} python2 -c "from builds.GpBuild import GpBuild; GpBuild(\"planner\").create_demo_cluster(install_dir='/usr/local/gpdb')"
 }
 
 function configure_with_planner() {
