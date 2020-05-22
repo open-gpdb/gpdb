0e930b4fbf4e1b3e6b6bed5ba78d1944beb35ed8
Huiliang.liu <huliu@pivotal.io>
2020-05-22 11:52:52 +0800

    Setup GPHOME_LOADERS environment and script (#10107) (#10162)
    GPHOME_LOADERS and greenplum_loaders_path.sh are still requried by some users.
    So export GPHOME_LOADERS as GPHOME_CLIENTS and link greenplum_loaders_path.sh
    to greenplum_clients_path.sh for compatible
    
    (Cherry-pick from master commit 9e9917abdbbc3cfc09259b9f390bee93b9403d44)

--- concourse/scripts/compile_gpdb.bash
+++ concourse/scripts/compile_gpdb.bash
@@ -207,6 +207,9 @@ function export_gpdb_clients() {
     cp ${GREENPLUM_INSTALL_DIR}/lib/python/gppylib/__init__.py ./bin/ext/gppylib
     cp  ${GREENPLUM_INSTALL_DIR}/lib/python/gppylib/gpversion.py ./bin/ext/gppylib
     python -m compileall -q -x test .
+    # GPHOME_LOADERS and greenplum_loaders_path.sh are still requried by some users
+    # So link greenplum_loaders_path.sh to greenplum_clients_path.sh for compatible
+    ln -sf greenplum_clients_path.sh greenplum_loaders_path.sh
     chmod -R 755 .
     tar -czf "${TARBALL}" ./*
   popd
