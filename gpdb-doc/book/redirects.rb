ebdc6e9ae53f779e74aa0cb85e0fb8517674d4c0
David Yozie <dyozie@pivotal.io>
2020-06-19 14:28:46 -0700

    Docs - updating versioning to 6.9

--- gpdb-doc/book/redirects.rb
+++ gpdb-doc/book/redirects.rb
@@ -1,4 +1,4 @@
-r301 '/', '/6-8/common/gpdb-features.html'
-r301 '/index.html', '/6-8/common/gpdb-features.html'
-r301 '/6-8/index.html', '/6-8/common/gpdb-features.html'
-r301 %r{(.*)/homenav.html}, '/6-8/common/gpdb-features.html'
+r301 '/', '/6-9/common/gpdb-features.html'
+r301 '/index.html', '/6-9/common/gpdb-features.html'
+r301 '/6-9/index.html', '/6-9/common/gpdb-features.html'
+r301 %r{(.*)/homenav.html}, '/6-9/common/gpdb-features.html'
