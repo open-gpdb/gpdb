73b477a012dcd0455f46963e7312100b901bc6ff
Hubert Zhang <hzhang@pivotal.io>
2020-09-02 10:10:17 +0800

    Using lwlock to protect resgroup slot in session state
    Resource group used to access resGroupSlot in SessionState without
    lock. This is correct when session only access resGroupSlot by itself.
    But as we introduced runaway feature, we need to traverse the current
    session array to find the top consumer session when redzone is reached.
    This requires:
    1. runaway detector should hold shared resgroup lock to avoid resGroupSlot
    is detached from a session concurrently when redzone is reached.
    2. normal session should hold exclusive lock when modifying resGroupSlot
    in SessionState.
    
    Also fix a compile warning.
    
    Reviewed-by: Ning Yu <nyu@pivotal.io>
    
    (cherry picked from commit a4cb06b4005b9a397361d6f678e1db6989972350)

--- src/backend/utils/resgroup/resgroup.c
+++ src/backend/utils/resgroup/resgroup.c
@@ -372,6 +372,7 @@ static void resgroupDumpSlots(StringInfo str);
 static void resgroupDumpFreeSlots(StringInfo str);
 
 static void sessionSetSlot(ResGroupSlotData *slot);
+static void sessionResetSlot(void);
 static ResGroupSlotData *sessionGetSlot(void);
 
 static void bindGroupOperation(ResGroupData *group);
@@ -2731,8 +2732,7 @@ UnassignResGroup(bool releaseSlot)
 		 * Reset resource group slot for current session. Note MySessionState
 		 * could be reset as NULL in shmem_exit() before.
 		 */
-		if (MySessionState != NULL)
-			MySessionState->resGroupSlot = NULL;
+		sessionResetSlot();
 	}
 
 	LWLockRelease(ResGroupLock);
@@ -3122,8 +3122,7 @@ groupWaitCancel(bool isMoveQuery)
 		 * Reset resource group slot for current session. Note MySessionState
 		 * could be reset as NULL in shmem_exit() before.
 		 */
-		if (MySessionState != NULL)
-			MySessionState->resGroupSlot = NULL;
+		sessionResetSlot();
 
 		group->totalExecuted++;
 
@@ -3858,7 +3857,37 @@ sessionSetSlot(ResGroupSlotData *slot)
 	Assert(slot != NULL);
 	Assert(MySessionState->resGroupSlot == NULL);
 
+	/*
+	 * SessionStateLock is required since runaway detector will traverse
+	 * the current session array and check corresponding resGroupSlot with
+	 * shared lock on SessionStateLock.
+	 */
+	LWLockAcquire(SessionStateLock, LW_EXCLUSIVE);
+
 	MySessionState->resGroupSlot = (void *) slot;
+
+	LWLockRelease(SessionStateLock);
+}
+
+/*
+ * Reset resource group slot for current session to NULL.
+ */
+static void
+sessionResetSlot(void)
+{
+	/*
+	 * SessionStateLock is required since runaway detector will traverse
+	 * the current session array and check corresponding resGroupSlot with
+	 * shared lock on SessionStateLock.
+	 */
+	if (MySessionState != NULL)
+	{
+		LWLockAcquire(SessionStateLock, LW_EXCLUSIVE);
+
+		MySessionState->resGroupSlot = NULL;
+
+		LWLockRelease(SessionStateLock);
+	}
 }
 
 /*
