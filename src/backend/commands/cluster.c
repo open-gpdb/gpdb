5f87e80ac9eb0e63e765555d44702a00282413dd
xiong-gang <gxiong@pivotal.io>
2020-09-04 10:44:33 +0800

    Fix a 'VACUUM FULL' bug
    When doing 'VACUUM FULL', 'swap_relation_files' updates the pg_class entry but
    not increase the command counter, so the later 'vac_update_relstats' will
    inplace update the 'relfrozenxid' and 'relhasindex' of the old tuple, when the
    transaction is interrupted and aborted on the QE after this, the old entry is
    corrupted.

--- src/backend/commands/cluster.c
+++ src/backend/commands/cluster.c
@@ -50,6 +50,7 @@
 #include "storage/predicate.h"
 #include "storage/smgr.h"
 #include "utils/acl.h"
+#include "utils/faultinjector.h"
 #include "utils/fmgroids.h"
 #include "utils/inval.h"
 #include "utils/lsyscache.h"
@@ -1625,18 +1626,16 @@ swap_relation_files(Oid r1, Oid r2, bool target_is_pg_class,
 							mapped_tables);
 	}
 
-	/* swap size statistics too, since new rel has freshly-updated stats */
-	if (swap_stats)
+	/* Send statistics from QE to QD */
+	if (Gp_role == GP_ROLE_EXECUTE && swap_stats && !IsSystemClass(r1, relform1))
 	{
 		rel = relation_open(r1, AccessShareLock);
 
-		vac_update_relstats(rel, relform1->relpages, relform1->reltuples,
-							relform1->relallvisible,
-							relform1->relhaspkey,
-							relform1->relfrozenxid,
-							relform1->relminmxid,
-							false,
-							true /* isvacuum */);
+		vac_send_relstats_to_qd(rel,
+								relform1->relpages,
+								relform1->reltuples,
+								relform1->relallvisible);
+
 		relation_close(rel, AccessShareLock);
 	}
 	/* Clean up. */
@@ -1697,6 +1696,8 @@ finish_heap_swap(Oid OIDOldHeap, Oid OIDNewHeap,
 						swap_stats,
 						is_internal,
 						frozenXid, cutoffMulti, mapped_tables);
+
+	SIMPLE_FAULT_INJECTOR("after_swap_relation_files");
 
 	/*
 	 * If it's a system catalog, queue an sinval message to flush all
