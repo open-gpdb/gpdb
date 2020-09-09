3219b79204817860cdff866ef9d4e075e45f54a1
David Yozie <dyozie@pivotal.io>
2020-09-08 18:54:28 -0700

    Docs - update versioning for 6.11 release

--- gpdb-doc/book/redirects.rb
+++ gpdb-doc/book/redirects.rb
@@ -1,5 +1,5 @@
-r301 '/', '/6-10/common/gpdb-features.html'
-r301 '/index.html', '/6-10/common/gpdb-features.html'
-r301 '/6-10/index.html', '/6-10/common/gpdb-features.html'
-r301 %r{(.*)/homenav.html}, '/6-10/common/gpdb-features.html'
+r301 '/', '/6-11/common/gpdb-features.html'
+r301 '/index.html', '/6-11/common/gpdb-features.html'
+r301 '/6-11/index.html', '/6-11/common/gpdb-features.html'
+r301 %r{(.*)/homenav.html}, '/6-11/common/gpdb-features.html's
 r301 '/pxf/5-13/installing_pxf.html', 'https://github.com/greenplum-db/pxf/blob/master/package/README.md'
