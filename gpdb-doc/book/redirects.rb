6f68a8b43b2c051a994e97eef2f7647d91cc9851
David Yozie <dyozie@pivotal.io>
2020-12-09 10:52:49 -0800

    Docs - updating doc build for 6.13 release

--- gpdb-doc/book/redirects.rb
+++ gpdb-doc/book/redirects.rb
@@ -1,5 +1,5 @@
-r301 '/', '/6-12/common/gpdb-features.html'
-r301 '/index.html', '/6-12/common/gpdb-features.html'
-r301 '/6-12/index.html', '/6-12/common/gpdb-features.html'
-r301 %r{(.*)/homenav.html}, '/6-12/common/gpdb-features.html's
+r301 '/', '/6-13/common/gpdb-features.html'
+r301 '/index.html', '/6-13/common/gpdb-features.html'
+r301 '/6-13/index.html', '/6-13/common/gpdb-features.html'
+r301 %r{(.*)/homenav.html}, '/6-13/common/gpdb-features.html's
 r301 '/pxf/5-13/installing_pxf.html', 'https://github.com/greenplum-db/pxf/blob/master/package/README.md'
