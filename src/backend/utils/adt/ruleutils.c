61855c55f75a201c6caf5fb37c10d0e93b6a7a37
Junfeng(Jerome) Yang <yjerome@vmware.com>
2021-02-05 12:02:09 +0800

    Revert "Hold AccessShareLock in pg_get_expr()"
    This reverts commit 755a2e809ab89daa5495056c0f83072c1a61678f.

--- src/backend/utils/adt/ruleutils.c
+++ src/backend/utils/adt/ruleutils.c
@@ -1844,9 +1844,7 @@ pg_get_expr(PG_FUNCTION_ARGS)
 	text	   *expr = PG_GETARG_TEXT_P(0);
 	Oid			relid = PG_GETARG_OID(1);
 	int			prettyFlags;
-	char		*relname;
-	text		*result;
-	Relation	rel;
+	char	   *relname;
 
 	prettyFlags = PRETTYFLAG_INDENT;
 
@@ -1867,27 +1865,7 @@ pg_get_expr(PG_FUNCTION_ARGS)
 	else
 		relname = NULL;
 
-	/* 
-	 * CDB: hold the AccessShareLock in case some transactions drop it concurrently.
-	 *
-	 * Since here, if the table that the relid tells is dropped, an error will raise
-	 * later when opening the relation to get column names.
-	 *
-	 * pg_get_expr() is used by GPDB add-on view 'pg_partitions' which is widely
-	 * used by regression tests for partition tables. Lots of parallel test cases
-	 * issue view pg_partitions and drop partitions concurrently, so those cases
-	 * are very flaky. Serialize test cases will cost more testing time and be
-	 * fragile, so GPDB holds a AccessShareLock here to make tests stable.
-	 */
-	rel = try_relation_open(relid, AccessShareLock, false);
-
-	if (!rel)
-		PG_RETURN_NULL();
-
-	result = pg_get_expr_worker(expr, relid, relname, prettyFlags);
-	relation_close(rel, AccessShareLock);
-
-	PG_RETURN_TEXT_P(result);
+	PG_RETURN_TEXT_P(pg_get_expr_worker(expr, relid, relname, prettyFlags));
 }
 
 Datum
@@ -1897,9 +1875,7 @@ pg_get_expr_ext(PG_FUNCTION_ARGS)
 	Oid			relid = PG_GETARG_OID(1);
 	bool		pretty = PG_GETARG_BOOL(2);
 	int			prettyFlags;
-	char		*relname;
-	text		*result;
-	Relation	rel;
+	char	   *relname;
 
 	prettyFlags = pretty ? (PRETTYFLAG_PAREN | PRETTYFLAG_INDENT | PRETTYFLAG_SCHEMA) : PRETTYFLAG_INDENT;
 
@@ -1914,27 +1890,7 @@ pg_get_expr_ext(PG_FUNCTION_ARGS)
 	else
 		relname = NULL;
 
-	/* 
-	 * CDB: hold the AccessShareLock in case some transactions drop it concurrently.
-	 *
-	 * Since here, if the table that the relid tells is dropped, an error will raise
-	 * later when opening the relation to get column names.
-	 *
-	 * pg_get_expr() is used by GPDB add-on view 'pg_partitions' which is widely
-	 * used by regression tests for partition tables. Lots of parallel test cases
-	 * issue view pg_partitions and drop partitions concurrently, so those cases
-	 * are very flaky. Serialize test cases will cost more testing time and be
-	 * fragile, so GPDB holds a AccessShareLock here to make tests stable.
-	 */
-	rel = try_relation_open(relid, AccessShareLock, false);
-
-	if (!rel)
-		PG_RETURN_NULL();
-
-	result = pg_get_expr_worker(expr, relid, relname, prettyFlags);
-	relation_close(rel, AccessShareLock);
-
-	PG_RETURN_TEXT_P(result);
+	PG_RETURN_TEXT_P(pg_get_expr_worker(expr, relid, relname, prettyFlags));
 }
 
 static text *
