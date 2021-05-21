af7c79b4f936b9824a7bba4e31017d6a51ebba75
Shreedhar Hardikar <shardikar@vmware.com>
2021-05-21 16:46:36 -0500

    Fix wrong results from nested LOJ in ALL subqueries
    NOT IN is equivalent to "<> ALL" SQL expression and should be replaced
    with LAS-Apply-NotIn in CSubqueryHandler::FRemoveAllSubquery().
    
    Earlier, ORCA transformed "<> ALL" subquery to a LAS-Apply-NotIn with the apply
    predicate as a filter over it's inner child (See CXformSelect2Apply). This is
    problematic when the subquery contains NULLs, as the generated filter is NULL
    rejecting. Thus, although LAS-Apply-NotIn correctly handles the NULL-semantics
    of a NOT IN subquery, an incorrect NULL rejecting filter on inner side will
    prevent the Apply from seeing any NULLs, resulting in wrong results.
    
    For example: A LOJ inside the NOT IN subquery (see issue #10967)
    
    create table t1(a int) distributed by (a);
    insert into t1 values (1), (2), (3);
    set optimizer_join_order='query';
    select * from t1 where a not in (
        select b.a from t1 a left join t1 b on false);
    
    ORCA transforms the following SubqueryAll operator:
    
    +--CScalarSubqueryAll(<>)["a" (16)]
        |--CLogicalLeftOuterJoin
        |  |--CLogicalGet "t1" ("t1")
        |  |--CLogicalConstTableGet
        |  +--CScalarConst (1)
        +--CScalarIdent "a" (0)
    
    to a LAS-Apply-NotIn, with a filter over it's inner child:
    
    +--CLogicalLeftAntiSemiApplyNotIn (Reqd Inner Cols: "a" (16))
        |--CLogicalGet "t1" ("t1")
        |--CLogicalSelect
        |  |--CLogicalLeftOuterJoin
        |  |  |--CLogicalGet "t1" ("t1"),
        |  |  |--CLogicalConstTableGet
        |  |  +--CScalarConst (1)
        |  +--CScalarCmp (=)
        |     |--CScalarIdent "a" (0)
        |     +--CScalarIdent "a" (16)
        +--CScalarConst (1)
    
    The filter is NULL rejecting, and thus the inner Left Outer Join is
    normalized to an Inner Join:
    
    +--CLogicalLeftAntiSemiApplyNotIn (Reqd Inner Cols: "b" (16))
       |--CLogicalGet "t1" ("t1")
       |--CLogicalInnerJoin
       |  |--CLogicalGet "t2" ("t2")
       |  |--CLogicalConstTableGet
       |  +--CScalarCmp (=)
       |     |--CScalarIdent "a" (0)
       |     +--CScalarIdent "b" (16)
       +--CScalarConst (1)
    
    The Apply operator sees an empty set on the inner side, thus produce the
    incorrect output of : (1), (2), (3).
    
    This commit fixes issue #10967, by placing the predicate in the
    LAS-Apply-NotIn, instead of generating a NULL rejecting filter:
    
    +--CLogicalLeftAntiSemiApplyNotIn (Reqd Inner Cols: "b" (16))
       |--CLogicalGet "t1" ("t1")
       |--CLogicalLeftOuterJoin
       |  |--CLogicalGet "t2" ("t2")
       |  |--CLogicalConstTableGet
       |  +--CScalarConst (1)
       +--CScalarCmp (=)
          |--CScalarIdent "a" (0)
          +--CScalarIdent "b" (16)

--- src/backend/gporca/libgpopt/src/xforms/CSubqueryHandler.cpp
+++ src/backend/gporca/libgpopt/src/xforms/CSubqueryHandler.cpp
@@ -1618,7 +1618,6 @@ CSubqueryHandler::FRemoveAllSubquery(CExpression *pexprOuter,
 
 	BOOL fSuccess = true;
 	BOOL fUseCorrelated = false;
-	CExpression *pexprInnerSelect = NULL;
 	CExpression *pexprPredicate = NULL;
 	CExpression *pexprInner = (*pexprSubquery)[0];
 	COperator::EOperatorId eopidSubq = pexprSubquery->Pop()->Eopid();
@@ -1654,13 +1653,11 @@ CSubqueryHandler::FRemoveAllSubquery(CExpression *pexprOuter,
 		}
 	}
 
-	CExpression *pexprInversePred =
-		CXformUtils::PexprInversePred(mp, pexprSubquery);
 	// generate a select with the inverse predicate as the selection predicate
 	// TODO: Handle the case where pexprInversePred == NULL
+	CExpression *pexprInversePred =
+		CXformUtils::PexprInversePred(mp, pexprSubquery);
 	pexprPredicate = pexprInversePred;
-	pexprInnerSelect =
-		CUtils::PexprLogicalSelect(mp, pexprInner, pexprPredicate);
 
 	if (EsqctxtValue == esqctxt)
 	{
@@ -1679,11 +1676,8 @@ CSubqueryHandler::FRemoveAllSubquery(CExpression *pexprOuter,
 				fUseCorrelated = true;
 		}
 
-		CExpression *pexprNewInnerSelect = PexprInnerSelect(
+		CExpression *pexprInnerSelect = PexprInnerSelect(
 			mp, colref, pexprInner, pexprPredicate, &fUseNotNullOptimization);
-
-		pexprInnerSelect->Release();
-		pexprInnerSelect = pexprNewInnerSelect;
 
 		if (!fUseCorrelated)
 		{
@@ -1699,6 +1693,10 @@ CSubqueryHandler::FRemoveAllSubquery(CExpression *pexprOuter,
 				mp, pexprOuter, pexprSubquery, esqctxt, ppexprNewOuter,
 				ppexprResidualScalar);
 		}
+
+		// cleanup
+		pexprInner->Release();
+		pexprPredicate->Release();
 	}
 	else
 	{
@@ -1707,7 +1705,7 @@ CSubqueryHandler::FRemoveAllSubquery(CExpression *pexprOuter,
 		*ppexprResidualScalar = CUtils::PexprScalarConstBool(mp, true);
 		*ppexprNewOuter =
 			CUtils::PexprLogicalApply<CLogicalLeftAntiSemiApplyNotIn>(
-				mp, pexprOuter, pexprInnerSelect, colref, eopidSubq);
+				mp, pexprOuter, pexprInner, colref, eopidSubq, pexprPredicate);
 	}
 
 	return fSuccess;
