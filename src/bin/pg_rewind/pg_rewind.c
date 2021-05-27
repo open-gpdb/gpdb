a6c1730a643749fb8b648a4b5ec927444faa28f6
Paul Guo <paulguo@gmail.com>
2021-05-27 10:16:46 +0800

    Backport fsync_fname() and fsync_parent_path() for pg_rewind use.
    The main difference is that we print more friendly message for errno ENOENT
    else pg_rewind log may confuse users since such cases are usually expected
    because either the delete sequence in the filemap causes, or the file has been
    explicitly deleted after rewinding.
    
    e.g. "pg_replslot/internal_wal_replication_slot" or "recovery.done".
    
    Reviewed by: Jimmy Yih <jyih@pivotal.io>

--- src/bin/pg_rewind/pg_rewind.c
+++ src/bin/pg_rewind/pg_rewind.c
@@ -643,6 +643,95 @@ updateControlFile(ControlFileData *ControlFile)
 }
 
 /*
+ * fsync_fname -- Try to fsync a file or directory
+ *
+ * Ignores errors trying to open unreadable files, or trying to fsync
+ * directories on systems where that isn't allowed/required.  Reports
+ * other errors non-fatally.
+ */
+static int
+fsync_fname(const char *fname, bool isdir)
+{
+	int			fd;
+	int			flags;
+	int			returncode;
+
+	/*
+	 * Some OSs require directories to be opened read-only whereas other
+	 * systems don't allow us to fsync files opened read-only; so we need both
+	 * cases here.  Using O_RDWR will cause us to fail to fsync files that are
+	 * not writable by our userid, but we assume that's OK.
+	 */
+	flags = PG_BINARY;
+	if (!isdir)
+		flags |= O_RDWR;
+	else
+		flags |= O_RDONLY;
+
+	/*
+	 * Open the file, silently ignoring errors about unreadable files (or
+	 * unsupported operations, e.g. opening a directory under Windows), and
+	 * logging others.
+	 */
+	fd = open(fname, flags, 0);
+	if (fd < 0)
+	{
+		if (errno == EACCES || (isdir && errno == EISDIR))
+			return 0;
+		else if (errno == ENOENT)
+			printf("skip sync-ing file \"%s\" since it does not exist (usually expected)\n", fname);
+		else
+			printf("could not open file \"%s\": %m\n", fname);
+		return -1;
+	}
+
+	returncode = fsync(fd);
+
+	/*
+	 * Some OSes don't allow us to fsync directories at all, so we can ignore
+	 * those errors. Anything else needs to be reported.
+	 */
+	if (returncode != 0 && !(isdir && (errno == EBADF || errno == EINVAL)))
+	{
+		printf("could not fsync file \"%s\": %m\n", fname);
+		(void) close(fd);
+		return -1;
+	}
+
+	(void) close(fd);
+	return 0;
+}
+
+/*
+ * fsync_parent_path -- fsync the parent path of a file or directory
+ *
+ * This is aimed at making file operations persistent on disk in case of
+ * an OS crash or power failure.
+ */
+static int
+fsync_parent_path(const char *fname)
+{
+	char		parentpath[MAXPGPATH];
+
+	strlcpy(parentpath, fname, MAXPGPATH);
+	get_parent_directory(parentpath);
+
+	/*
+	 * get_parent_directory() returns an empty string if the input argument is
+	 * just a file name (see comments in path.c), so handle that as being the
+	 * current directory.
+	 */
+	if (strlen(parentpath) == 0)
+		strlcpy(parentpath, ".", MAXPGPATH);
+
+	if (fsync_fname(parentpath, true) != 0)
+		return -1;
+
+	return 0;
+}
+
+
+/*
  * Sync target data directory to ensure that modifications are safely on disk.
  *
  * gpdb: We assume that all files are synchronized before rewinding and thus we
@@ -661,10 +750,7 @@ syncTargetDirectory()
 	int			  i;
 
 	if (chdir(datadir_target) < 0)
-	{
-		pg_log_error("could not change directory to \"%s\": %m", datadir_target);
-		exit(1);
-	}
+		pg_fatal("could not change directory to \"%s\": %m", datadir_target);
 
 	for (i = 0; i < filemap->narray; i++)
 	{
