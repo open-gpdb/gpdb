open-gpdb/gpdb

commit bdad273d27da8dd6c6cf757da7ed150ed39ea2c5

Dump extension functions under pg_catalog for 5X
This ensures that when pg_dump is invoked in binary_upgrade mode against
a 5X cluster during pg_upgrade, it collects and dumps the functions
under pg_catalog belonging to an extension.

We found this bug when we tried to upgrade PXF and PXF's functions did
not get dumped in binary upgrade mode. (PXF's functions are created under
pg_catalog)

This change in 6X should have accompanied the backport of the extension
machinery into 5X.

Co-authored-by: Kalen Krempely <kkrempely@vmware.com>

src/bin/pg_dump/pg_dump.c
@@ -5035,7 +5035,14 @@ getFuncs(Archive *fout, int *numFuncs)
 							 "\n  WHERE pg_cast.oid > '%u'::oid"
 							 "\n  AND p.oid = pg_cast.castfunc)",
 							 FirstNormalObjectId);
-		if (binary_upgrade && fout->remoteVersion >= 90100)
+/*
+ * GPDB: Much of the extension machinery was backported into GPDB 5 from higher
+ * major versions. So include the clause if we are running against GPDB 5.
+ */
+#if 0
+		if (binary_upgrade && fout->remoteVersion >= 90200)
+#endif
+		if (binary_upgrade && fout->remoteVersion >= 80300)
 			appendPQExpBufferStr(query,
 							   "\n  OR EXISTS(SELECT 1 FROM pg_depend WHERE "
 								 "classid = 'pg_proc'::regclass AND "

commit 82286928f706c11184f7c6eddabb3117bef9f0d0

pg_dump: Fix typo in #if-0ed version check
The typo was introduced in bdad273

Co-authored-by: Kalen Krempely <kkrempely@vmware.com>

src/bin/pg_dump/pg_dump.c
@@ -5040,7 +5040,7 @@ getFuncs(Archive *fout, int *numFuncs)
  * major versions. So include the clause if we are running against GPDB 5.
  */
 #if 0
-		if (binary_upgrade && fout->remoteVersion >= 90200)
+		if (binary_upgrade && fout->remoteVersion >= 90100)
 #endif
 		if (binary_upgrade && fout->remoteVersion >= 80300)
 			appendPQExpBufferStr(query,