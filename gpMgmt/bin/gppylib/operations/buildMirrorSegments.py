da00cdea0b4e50bdf93577c2ef636c480f9a17c3
Nikhil Kak <nkak@vmware.com>
2021-12-10 17:43:56 -0800

    recoverseg/addmirrors/movemirrors: Update catalog before recovery
    A previous commit added code to start the segment as soon as it has
    successfully recovered. For the segment to be marked as 'up', we also
    need to make sure that any catalog updates(if any) are done before
    trying to start the segment. This commit updates the catalog before
    attempting to recover.
    
    If any of the segments fails during the recovery process, we would also
    have to make sure that we undo any catalog changes for that segment.
    This will be taken care of in a future commit.

--- gpMgmt/bin/gppylib/operations/buildMirrorSegments.py
+++ gpMgmt/bin/gppylib/operations/buildMirrorSegments.py
@@ -251,8 +251,6 @@ class GpMirrorListToBuild:
 
         self._validate_gparray(gpArray)
 
-        self._run_recovery(gpEnv)
-
         # should use mainUtils.getProgramName but I can't make it work!
         programName = os.path.split(sys.argv[0])[-1]
 
@@ -270,6 +268,8 @@ class GpMirrorListToBuild:
         finally:
             # Reenable Ctrl-C
             signal.signal(signal.SIGINT, signal.default_int_handler)
+
+        self._run_recovery(gpEnv)
 
         #TODO when should this return False ? when basebackup/rewind/start fail for even one segment ?
         return True
