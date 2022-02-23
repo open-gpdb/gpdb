d07ecfbb4f57c1ef816efed45ee0e1bd5928aa75
Huansong Fu <fuhuansong@gmail.com>
2022-02-23 13:06:47 -0800

    Revert "Fix index corruption when invalid snapshot used with AO tables. (#12797)"
    This reverts commit 44dc41a212b98b7b60c470f8f85ffc72894526fe.
    
    This introduced a compile error on pipeline when -Werror=maybe-uninitialized
    is given. We somehow missed the pipeline check in the original PR.
    
    The fix should be simple but since this is blocking release 6.19.3 now and
    it takes time to verify the fix on pipeline, we'll just revert it and
    re-submit a fixed version of the commit.

--- src/backend/catalog/index.c
+++ src/backend/catalog/index.c
@@ -2529,13 +2529,11 @@ IndexBuildScan(Relation parentRelation,
 	 * concurrent build, or during bootstrap, we take a regular MVCC snapshot
 	 * and index whatever's live according to that.
 	 *
-	 * Like for heap tables, if the relation is an append-only table, we use
-	 * SnapshotAny to access data. RECENTLY_DEAD tuples, used, for example, by
-	 * "repeatable read" transaction, should be indexed to avoid inconsistent
-	 * results caused by index access. We use another snapshot to access
-	 * metadata - see AO-specific scan functions implementation below.
+	 * If the relation is an append-only table, we also use a regular MVCC
+	 * snapshot.
 	 */
-	if (IsBootstrapProcessingMode() || indexInfo->ii_Concurrent)
+	if (IsBootstrapProcessingMode() || indexInfo->ii_Concurrent ||
+			 RelationIsAppendOptimized(parentRelation))
 	{
 		snapshot = RegisterSnapshot(GetTransactionSnapshot());
 		registered_snapshot = true;
@@ -2544,13 +2542,8 @@ IndexBuildScan(Relation parentRelation,
 	else
 	{
 		snapshot = SnapshotAny;
-		/*
-		 * TODO: add support to cut off definitely dead rows using OldestXmin
-		 * for AO tables
-		 */
-		if (!RelationIsAppendOptimized(parentRelation))
-			/* okay to ignore lazy VACUUMs here */
-			OldestXmin = GetOldestXmin(parentRelation, true);
+		/* okay to ignore lazy VACUUMs here */
+		OldestXmin = GetOldestXmin(parentRelation, true);
 	}
 
 	if (RelationIsHeap(parentRelation))
@@ -3006,16 +2999,9 @@ IndexBuildAppendOnlyRowScan(Relation parentRelation,
 	predicate = (List *)
 		ExecPrepareExpr((Expr *)indexInfo->ii_Predicate, estate);
 	
-	/*
-	 * Regular MVCC snapshot, which may be taken in IndexBuildScan function,
-	 * can hide newly globally inserted tuples from global index build process,
-	 * because it's outdated in such case (see dispatching part of DefineIndex
-	 * for more). Since this, we need to use SnapshotSelf to get the actual
-	 * metadata (pg_aoseg, in particular)".
-	 */
 	aoscan = appendonly_beginscan(parentRelation,
 								  snapshot,
-								  SnapshotSelf,
+								  snapshot,
 								  0,
 								  NULL);
 
@@ -3155,18 +3141,7 @@ IndexBuildAppendOnlyColScan(Relation parentRelation,
 			proj[attno] = true;
 	}
 	
-	/*
-	 * Regular MVCC snapshot, which may be taken in IndexBuildScan function,
-	 * can hide newly globally inserted tuples from global index build process,
-	 * because it's outdated in such case (see dispatching part of DefineIndex
-	 * for more). Since this, we need to use SnapshotSelf to get the actual
-	 * metadata (pg_aoseg, in particular).
-	 */
-	aocsscan = aocs_beginscan(parentRelation,
-							  snapshot,
-							  SnapshotSelf,
-							  NULL /* relationTupleDesc */,
-							  proj);
+	aocsscan = aocs_beginscan(parentRelation, snapshot, snapshot, NULL /* relationTupleDesc */, proj);
 
 	if (!OidIsValid(blkdirrelid) || !OidIsValid(blkdiridxid))
 	{
