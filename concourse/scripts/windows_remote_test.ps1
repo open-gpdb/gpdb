95118dcf152bee6f5922a6e418679421a7de2d93
Ed Espino <eespino@vmware.com>
2022-08-24 00:43:52 -0700

    Use pre-installed VC_redist.x64.exe artifact.

--- concourse/scripts/windows_remote_test.ps1
+++ concourse/scripts/windows_remote_test.ps1
@@ -1,5 +1,3 @@
 $progressPreference = 'silentlyContinue'
-copy C:\vcredist\VC_redist.x64.exe VC_redist.x64.exe
-Start-Process -FilePath "VC_redist.x64.exe" -ArgumentList "/passive" -Wait -Passthru
 Start-Process msiexec.exe -Wait -ArgumentList '/I greenplum-clients-x86_64.msi /quiet'
 $env:PATH="C:\Program Files\Greenplum\greenplum-clients\bin;C:\Program Files\curl-win64-mingw\bin;" + $env:PATH
