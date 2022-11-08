bcf5160a2a485f2e6e6c3b452a0fc9eeb2fa69b7
Soumyadeep Chakraborty <soumyadeep2007@gmail.com>
2022-11-08 14:08:40 -0800

    ic-proxy: Fix cache eviction on remote peer shutdown
    This is a backport of main commit:
    354a7b6dffd00e46470f472fdf552be82e3c1f49
    
    Conflicts:
    * Minor conflict due to differences in struct BackgroundWorker.
    
    Original commit message follows:
    
    Upon remote peer shutdown, we need to drop all logical connections
    involving the remote peer. A part of that is dropping packets that were
    cached by placeholder clients.
    
    Unfortunately, we were indiscriminately dropping these for clients
    belonging to ALL logical connections (regardless of the dbid of the
    remote peer) in ic_proxy_client_table_shutdown_by_dbid().
    
    This can lead to DATA and BYE packets being dropped from the client-local
    cache for unrelated connections, leading to messages like:
    
    WARNING: ic-proxy: client.placeholder[...]: unhandled cached BYE [..], dropping it
    
    Consequently, later when the placeholder clients are replaced by actual
    clients, these packets aren't transferred and ultimately are never
    processed, leading to messages like:
    DEBUG3: ic-proxy: [...]: replace my placeholder client.placeholder[...]
    DEBUG3: ic-proxy: [...]: no cached pkts in the placeholder client.placeholder[...]
    
    If BYEs are dropped like this and left unhandled, it will lead to hangs
    on the remote side in waitForOutbound(). The test added highlights this
    case.
    
    We fix this by dropping cached packets ONLY for clients whose remote dbid
    matches the remote peer's dbid.

--- src/backend/postmaster/postmaster.c
+++ src/backend/postmaster/postmaster.c
@@ -421,7 +421,11 @@ static BackgroundWorker PMAuxProcList[MaxPMAuxProc] =
 
 #ifdef ENABLE_IC_PROXY
 	{"ic proxy process",
+#ifdef FAULT_INJECTOR
+	 BGWORKER_SHMEM_ACCESS,
+#else
 	 0,
+#endif
 	 BgWorkerStart_RecoveryFinished,
 	 0, /* restart immediately if ic proxy process exits with non-zero code */
 	 ICProxyMain, {0}, {0}, 0, 0,
