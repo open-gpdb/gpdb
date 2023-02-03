7d0c98f593094079dda9234f8a853b77bab7535b
Huansong Fu <fuhuansong@gmail.com>
2023-02-03 12:32:04 -0800

    Drop metadata tracking info when dropping schemas
    Backport from main branch 03a1560ece88d01c65d3ead18f018efbe55b687c
    
    Due to some test flakiness, we found that when we drop a schema,
    we were not dropping the meta tracking rows in pg_stat_last_operation
    associated with the schema. We used to do that in RemoveSchema:
    c83fb8108854. But it was lost in 4750e1b65518a5575e0cc75eb6716cc7d5594139
    which got rid of RemoveSchema and only used RemoveSchemaById ever since.
    
    Now add back MetaTrackDropObject in RemoveSchemaById. Add test case too.

--- src/test/regress/sql/pg_stat_last_operation.sql
+++ src/test/regress/sql/pg_stat_last_operation.sql
@@ -10,6 +10,18 @@ FROM pg_stat_operations WHERE schemaname = 'public'
 AND objname = 'pg_stat_last_operation_test'
 AND actionname = 'TRUNCATE';
 
+CREATE VIEW
+	pg_stat_last_operation_testview_schema AS
+SELECT lo.staactionname,
+       lo.stasubtype,
+       ns.nspname
+FROM   pg_stat_last_operation lo
+       join pg_class c
+         ON lo.classid = c.oid
+       join pg_namespace ns
+         ON c.relname = 'pg_namespace'
+            AND lo.objid = ns.oid;
+
 insert into PG_STAT_LAST_OPERATION_TEST select generate_series(1,100);
 
 truncate PG_STAT_LAST_OPERATION_TEST;
@@ -18,3 +30,9 @@ SELECT classname, objname, schemaname, actionname, subtype
 FROM pg_stat_operations WHERE schemaname = 'public' 
 AND objname = 'pg_stat_last_operation_test'
 AND actionname = 'TRUNCATE';
+
+-- CREATE/DROP SCHEMA
+CREATE SCHEMA mdt_schema;
+SELECT * FROM pg_stat_last_operation_testview_schema WHERE nspname LIKE 'mdt_%';
+DROP SCHEMA mdt_schema;
+SELECT * FROM pg_stat_last_operation_testview_schema WHERE nspname LIKE 'mdt_%';
