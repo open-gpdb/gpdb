b91524624442cd261f248f8beed8a9b6042bdd00
wenru yan <ywenru@vmware.com>
2023-07-26 02:38:39 +0000

    [6X] minirepro: collect using sequence
    When a table has an explicitly created sequence, the sequence will
    be ignored by pg_dump if it's not owned by the table.
    
    In pg_dump, implicitly created sequence like SERIAL and GENERATED
    AS IDENTITY, the referenced sequence will be dumped if the owning
    table is dumped. But for explicitly created sequence, we should
    specify sequence's oid if we want to dump the sequence object when
    the sequence is not owned by table.
    
    Collect oids of sequence which be referenced by table columns in
    gp_dump_query_oids.

--- src/test/regress/sql/minirepro.sql
+++ src/test/regress/sql/minirepro.sql
@@ -3,7 +3,8 @@ drop database if exists gpsd_db_with_hll;
 -- end_ignore
 create database gpsd_db_with_hll;
 \c gpsd_db_with_hll
-create table minirepro_foo(a int, b int[]);
+create sequence minirepro_foo_c_seq cache 1;
+create table minirepro_foo(a int, b int[], c int default nextval('minirepro_foo_c_seq'::regclass), d serial);
 
 insert into minirepro_foo values(1, '{10000, 10000, 10000, 10000}');
 insert into minirepro_foo values(1, '{10000, 10000, 10000, 10000}');
@@ -16,6 +17,7 @@ analyze minirepro_foo;
 -- end_ignore
 
 drop table minirepro_foo;
+drop sequence minirepro_foo_c_seq;
 
 \! psql -f data/minirepro.sql gpsd_db_with_hll
 
