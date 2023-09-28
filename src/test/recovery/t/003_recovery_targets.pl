tvesely/yugabyte-db

Fail if recovery target is not reached
Before, if a recovery target is configured, but the archive ended
before the target was reached, recovery would end and the server would
promote without further notice.  That was deemed to be pretty wrong.
With this change, if the recovery target is not reached, it is a fatal
error.

Based-on-patch-by: Leif Gunnar Erlandsen <leif@lako.no>
Reviewed-by: Kyotaro Horiguchi <horikyota.ntt@gmail.com>
Discussion: https://www.postgresql.org/message-id/flat/993736dd3f1713ec1f63fc3b653839f5@lako.no

src/postgres/src/test/recovery/t/003_recovery_targets.pl
@@ -3,7 +3,8 @@ use strict;
 use warnings;
 use PostgresNode;
 use TestLib;
-use Test::More tests => 8;
+use Test::More tests => 9;
+use Time::HiRes qw(usleep);
 
 # Create and test a standby from given backup, with a certain recovery target.
 # Choose $until_lsn later than the transaction commit that causes the row
@@ -145,3 +146,24 @@ ok(!$res, 'invalid recovery startup fails');
 my $logfile = slurp_file($node_standby->logfile());
 ok($logfile =~ qr/multiple recovery targets specified/,
 	'multiple conflicting settings');
+
+# Check behavior when recovery ends before target is reached
+
+$node_standby = get_new_node('standby_8');
+$node_standby->init_from_backup($node_master, 'my_backup',
+								has_restoring => 1, standby => 0);
+$node_standby->append_conf('postgresql.conf',
+						   "recovery_target_name = 'does_not_exist'");
+
+run_log(['pg_ctl', '-D', $node_standby->data_dir,
+		 '-l', $node_standby->logfile, 'start']);
+
+# wait up to 10 seconds for postgres to terminate
+foreach my $i (0..100)
+{
+	last if ! -f $node_standby->data_dir . '/postmaster.pid';
+	usleep(100_000);
+}
+$logfile = slurp_file($node_standby->logfile());
+ok($logfile =~ qr/FATAL:  recovery ended before configured recovery target was reached/,
+	'recovery end before target reached is a fatal error');

open-gpdb/gpdb

Backported from GPDB7:
https://github.com/greenplum-db/gpdb/commit/a93ab09e380a62240e8ea99d1c17f5385def8673
with the following changes:
 - In the TAP test framework added the `standby` parameter
   for the enable_restoring function to distinguish
   between standby and recovery which in GPDB7 is done
   via .signal files.
 - In the TAP test put recovery_target_name into recovery.conf
   file instead of postgresql.conf
 - In the TAP test added variables declaration.
Original GPDB7 commit message:
Backported from upstream with change in the test:
diff:
- run_log(['pg_ctl', '-D', $node_standby->data_dir,
-		 '-l', $node_standby->logfile, 'start']);
+ run_log(['pg_ctl', '-D', $node_standby->data_dir, '-l',
		$node_standby->logfile, '-o', "-c gp_role=utility --gp_dbid=$node_standby->{_dbid} --gp_contentid=0 -c maintenance_mode=on",
			'start']);
Original Postgres commit on REL_13_BETA1:
https://github.com/postgres/postgres/commit/dc788668bb269b10a108e87d14fefd1b9301b793
Original Postgres commit message:

src/test/recovery/t/003_recovery_targets.pl
@@ -3,7 +3,8 @@ use strict;
 use warnings;
 use PostgresNode;
 use TestLib;
-use Test::More tests =>6;
+use Test::More tests =>7;
+use Time::HiRes qw(usleep);
 
 # Create and test a standby from given backup, with a certain recovery target.
 # Choose $until_lsn later than the transaction commit that causes the row
@@ -128,3 +129,24 @@ test_recovery_standby('multiple overriding settings',
 	"recovery_target_time = '$recovery_time'");
 test_recovery_standby('multiple conflicting settings',
 	'standby_6', $node_master, \@recovery_params, "3000", $lsn3);
+
+# Check behavior when recovery ends before target is reached
+
+my $node_standby = get_new_node('standby_8');
+$node_standby->init_from_backup($node_master, 'my_backup',
+								has_restoring => 1, standby => 0);
+$node_standby->append_conf('recovery.conf',
+						   "recovery_target_name = 'does_not_exist'");
+run_log(['pg_ctl', '-w', '-D', $node_standby->data_dir, '-l',
+		$node_standby->logfile, '-o', "-c gp_role=utility --gp_dbid=$node_standby->{_dbid} --gp_contentid=0",
+			'start']);
+
+# wait up to 10 seconds for postgres to terminate
+foreach my $i (0..100)
+{
+	last if ! -f $node_standby->data_dir . '/postmaster.pid';
+	usleep(100_000);
+}
+my $logfile = slurp_file($node_standby->logfile());
+ok($logfile =~ qr/FATAL:  recovery ended before configured recovery target was reached/,
+	'recovery end before target reached is a fatal error');