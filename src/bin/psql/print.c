aa5d4fc5e49e7781fe4da70f68b92261f91274d5
Hongxu Ma <interma@outlook.com>
2023-11-28 11:43:48 +0800

    Fix int overflow in printTableAddCell() (#16495)
    When user try to select a big number of rows via PSQL, they may encounter the following error:
    ERROR: "Cannot add cell to table content: total cell count of XXX exceeded".
    The root cause is the overflow of int type in printTableAddCell(), this commit fixes it.
    
    And it has already been fixed it in 7x and postgres:
    - https://github.com/greenplum-db/gpdb/pull/16388
    - https://github.com/postgres/postgres/commit/24ea53dcfab7fe463c0cf56095e9fe5d23cdb748

--- src/bin/psql/print.c
+++ src/bin/psql/print.c
@@ -2301,6 +2301,8 @@ void
 printTableInit(printTableContent *const content, const printTableOpt *opt,
 			   const char *title, const int ncolumns, const int nrows)
 {
+	long total_cells;
+
 	content->opt = opt;
 	content->title = title;
 	content->ncolumns = ncolumns;
@@ -2308,7 +2310,8 @@ printTableInit(printTableContent *const content, const printTableOpt *opt,
 
 	content->headers = pg_malloc0((ncolumns + 1) * sizeof(*content->headers));
 
-	content->cells = pg_malloc0((ncolumns * nrows + 1) * sizeof(*content->cells));
+	total_cells = (long)ncolumns * (long)nrows;
+	content->cells = pg_malloc0((total_cells + 1) * sizeof(*content->cells));
 
 	content->cellmustfree = NULL;
 	content->footers = NULL;
@@ -2378,15 +2381,18 @@ void
 printTableAddCell(printTableContent *const content, char *cell,
 				  const bool translate, const bool mustfree)
 {
+	long total_cells;
 #ifndef ENABLE_NLS
 	(void) translate;			/* unused parameter */
 #endif
 
-	if (content->cellsadded >= content->ncolumns * content->nrows)
+	/* product of cols and rows, using long type to prevent the int overflow */
+	total_cells = (long)content->ncolumns * (long)content->nrows;
+	if (content->cellsadded >= total_cells)
 	{
 		fprintf(stderr, _("Cannot add cell to table content: "
-						  "total cell count of %d exceeded.\n"),
-				content->ncolumns * content->nrows);
+						  "total cell count of %ld exceeded, cells added: %ld.\n"),
+				total_cells, content->cellsadded);
 		exit(EXIT_FAILURE);
 	}
 
@@ -2402,7 +2408,7 @@ printTableAddCell(printTableContent *const content, char *cell,
 	{
 		if (content->cellmustfree == NULL)
 			content->cellmustfree =
-				pg_malloc0((content->ncolumns * content->nrows + 1) * sizeof(bool));
+				pg_malloc0((total_cells + 1) * sizeof(bool));
 
 		content->cellmustfree[content->cellsadded] = true;
 	}
@@ -2470,9 +2476,10 @@ printTableCleanup(printTableContent *const content)
 {
 	if (content->cellmustfree)
 	{
-		int			i;
-
-		for (i = 0; i < content->nrows * content->ncolumns; i++)
+		long		i;
+		long		total_cells;
+		total_cells = (long)content->ncolumns * (long)content->nrows;
+		for (i = 0; i < total_cells; i++)
 		{
 			if (content->cellmustfree[i])
 				free((char *) content->cells[i]);
