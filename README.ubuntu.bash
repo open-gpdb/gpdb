f1c642e9014e530f600b2fb86e52e4ccecaaf666
Victor <victorkremensky@mail.ru>
2024-11-11 09:06:34 +0000

    Install all (before : if is some packages missing didnt install any)

--- README.ubuntu.bash
+++ README.ubuntu.bash
@@ -1,43 +1,44 @@
 #!/bin/bash
 
 apt-get update
-apt-get install -y \
-  bison \
-  ccache \
-  cmake \
-  curl \
-  flex \
-  git-core \
-  gcc-8 \
-  g++-8 \
-  inetutils-ping \
-  krb5-kdc \
-  krb5-admin-server \
-  libapr1-dev \
-  libbz2-dev \
-  libcurl4-gnutls-dev \
-  libevent-dev \
-  libkrb5-dev \
-  libpam-dev \
-  libperl-dev \
-  libreadline-dev \
-  libssl-dev \
-  libxml2-dev \
-  libyaml-dev \
-  libzstd-dev \
-  locales \
-  net-tools \
-  ninja-build \
-  openssh-client \
-  openssh-server \
-  openssl \
-  python2-dev \
-  python3-pip \
-  python-psutil \
-  python-yaml \
-  zlib1g-dev \
-  libpstreams-dev
+apt-get update
+apt-get install -y  bison
+apt-get install -y  ccache 
+apt-get install -y  cmake 
+apt-get install -y  curl 
+apt-get install -y  flex
+apt-get install -y  git-core 
+apt-get install -y  gcc 
+apt-get install -y  g++ 
+apt-get install -y  inetutils-ping 
+apt-get install -y  krb5-kdc 
+apt-get install -y  krb5-admin-server 
+apt-get install -y  libapr1-dev 
+apt-get install -y  libbz2-dev 
+apt-get install -y  libcurl4-gnutls-dev 
+apt-get install -y  libevent-dev 
+apt-get install -y  libkrb5-dev 
+apt-get install -y  libpam-dev 
+apt-get install -y  libperl-dev 
+apt-get install -y  libreadline-dev 
+apt-get install -y  libssl-dev 
+apt-get install -y  libxml2-dev 
+apt-get install -y  libyaml-dev 
+apt-get install -y  libzstd-dev 
+apt-get install -y  locales 
+apt-get install -y  net-tools 
+apt-get install -y  ninja-build 
+apt-get install -y  openssh-client 
+apt-get install -y  openssh-server 
+apt-get install -y  openssl 
+apt-get install -y  python2-dev
+apt-get install -y  python-dev 
+apt-get install -y  python-pip 
+apt-get install -y  python-psutil 
+apt-get install -y  python-yaml 
+apt-get install -y  zlib1g-dev
+ln -s /bin/python2.7 /usr/bin/python
 
-
+pip2 install conan
 pip install conan
 
