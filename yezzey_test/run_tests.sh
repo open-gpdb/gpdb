02b44e99824e6f08ccd6a51f6e3b599d05bcf727
Yury Frolov <57130330+EinKrebs@users.noreply.github.com>
2025-04-03 14:50:31 +0300

    Use docker compose in yezzey tests (#172)

--- yezzey_test/run_tests.sh
+++ yezzey_test/run_tests.sh
@@ -67,4 +67,4 @@ i=0
 while (! [ -S /tmp/yproxy.sock ]) && [ $i -lt 20 ]; do sleep 1; i=$(($i+1)) ; done
 
 cd gpcontrib/yezzey
-make installcheck || (echo Yproxy logs; cat ../../yproxy.log; cat /home/gpadmin/gpcontrib/yezzey/regression.diffs && exit 1) 
+make installcheck || (echo Yproxy logs; cat /home/gpadmin/gpcontrib/yezzey/regression.diffs && exit 1)
