3bbbd033409bd1b69e0fd171920fa60547d60423
reshke <reshkekirill@gmail.com>
2025-04-23 12:36:23 +0500

    Algorithm S support funcs (#169)

--- src/backend/utils/misc/sampling.c
+++ src/backend/utils/misc/sampling.c
@@ -224,3 +224,81 @@ sampler_random_fract()
 {
 	return ((double) random() + 1) / ((double) MAX_RANDOM_VALUE + 2);
 }
+
+
+/*
+ * This is used for sampling AO/CO row numbers, in the flattened
+ * row number space, across all segfile tuple counts. 64 bits is
+ * used for simplicity and is sufficient to hold a maximum tuple
+ * count of (AOTupleId_MaxSegmentFileNum * MAX_AOREL_CONCURRENCY),
+ * which can be represented with 48 bits.
+ *
+ * The code is same as BlockSampler except replacing
+ * int type of variables with int64, which is intended
+ * to support larger size of the data set (N).
+ * 
+ * Duplicate code for not willing to break the original
+ * design to conflict with upstream for some special case.
+ */
+void
+RowSampler_Init(RowSampler rs, int64 nobjects, int64 samplesize,
+				long randseed)
+{
+	rs->N = nobjects;			/* measured table size */
+
+	/*
+	 * If we decide to reduce samplesize for tables that have less or not much
+	 * more than samplesize objects, here is the place to do it.
+	 */
+	rs->n = samplesize;
+	rs->t = 0;					/* objects scanned so far */
+	rs->m = 0;					/* objects selected so far */
+}
+
+bool
+RowSampler_HasMore(RowSampler rs)
+{
+	return (rs->t < rs->N) && (rs->m < rs->n);
+}
+
+int64
+RowSampler_Next(RowSampler rs)
+{
+	int64       K = rs->N - rs->t;	/* remaining objects */
+	int64		k = rs->n - rs->m;	/* objects still to sample */
+	double		p;				    /* probability to skip object */
+	double		V;				    /* random */
+
+	Assert(RowSampler_HasMore(rs));	/* hence K > 0 and k > 0 */
+
+	if (k >= K)
+	{
+		/* need all the rest */
+		rs->m++;
+		return rs->t++;
+	}
+
+    /* 
+     * It is not obvious that this code matches Knuth's Algorithm S.
+     * Refer to BlockSampler_Next() for detail.
+     */
+	V = sampler_random_fract();
+    /*
+	 * Don't bother overflow of conversion from int64 K (N) as it was
+	 * already converted to "double" range value when initialized.
+	 */
+	p = 1.0 - (double) k / (double) K;
+	while (V < p)
+	{
+		/* skip */
+		rs->t++;
+		K--; /* keep K == N - t */
+
+		/* adjust p to be new cutoff point in reduced range */
+		p *= 1.0 - (double) k / (double) K;
+	}
+
+	/* select */
+	rs->m++;
+	return rs->t++;
+}
--- src/include/cdb/cdbappendonlyam.h
+++ src/include/cdb/cdbappendonlyam.h
@@ -231,6 +231,34 @@ typedef struct AppendOnlyScanDescData
 	 */ 
 	AppendOnlyVisimap visibilityMap;
 
+	/*
+	 * used by `analyze`
+	 */
+
+	/*
+	 * targrow: the output of the Row-based sampler (Algorithm S), denotes a
+	 * rownumber in the flattened row number space that is the target of a sample,
+	 * which starts from 0.
+	 * In other words, if we have seg0 rownums: [1, 100], seg1 rownums: [1, 200]
+	 * If targrow = 150, then we are referring to seg1's rownum=51.
+	 */
+	int64				targrow;
+
+	/*
+	 * segfirstrow: pointing to the next starting row which is used to check
+	 * the distance to `targrow`
+	 */
+	int64				segfirstrow;
+
+	/*
+	 * segrowsprocessed: track the rows processed under the current segfile.
+	 * Don't miss updating it accordingly when "segfirstrow" is updated.
+	 */
+	int64				segrowsprocessed;
+
+	AOBlkDirScan		blkdirscan;
+
+
 }	AppendOnlyScanDescData;
 
 typedef AppendOnlyScanDescData *AppendOnlyScanDesc;
@@ -397,5 +425,23 @@ extern bool AppendOnlyExecutorReadBlock_ScanNextTuple(AppendOnlyExecutorReadBloc
 										  TupleTableSlot *slot);
 
 extern void AppendOnlyExecutorReadBlock_GetContents(AppendOnlyExecutorReadBlock *executorReadBlock);
+
+
+static inline int64
+AppendOnlyScanDesc_TotalTupCount(AppendOnlyScanDesc scan)
+{
+	Assert(scan != NULL);
+
+	int64 totalrows = 0;
+	FileSegInfo **seginfo = scan->aos_segfile_arr;
+
+    for (int i = 0; i < scan->aos_total_segfiles; i++)
+    {
+	    if (seginfo[i]->state != AOSEG_STATE_AWAITING_DROP)
+		    totalrows += seginfo[i]->total_tupcount;
+    }
+
+    return totalrows;
+}
 
 #endif   /* CDBAPPENDONLYAM_H */
