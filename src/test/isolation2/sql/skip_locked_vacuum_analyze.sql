cb03d765ee087c000ebecb16fdd40fa0171c4689
Victor Kremensky <victorkremensky@mail.ru>
2025-05-19 10:27:44 +0000

    stronger test

--- src/test/isolation2/sql/skip_locked_vacuum_analyze.sql
+++ src/test/isolation2/sql/skip_locked_vacuum_analyze.sql
@@ -5,9 +5,22 @@ DISTRIBUTED by (id)
 PARTITION by RANGE (id)
 (START (1) END (1000) EVERY (100), DEFAULT PARTITION extra);
 1: insert into tp (select generate_series(1,1000));
-1: analyze tp;
-1: insert into tp (select generate_series(1,1000));
 1: begin;
 1: lock table tp_1_prt_3;
 2: analyze SKIP_LOCKED tp;
 1: rollback;
+
+
+
+1: create table t1(id integer) 
+DISTRIBUTED by (id)
+PARTITION by RANGE (id)
+(START (1) END (1000) EVERY (100), DEFAULT PARTITION extra);
+1: insert into tp (select generate_series(1,400));
+1: begin;
+1: lock table tp_1_prt_3;
+2: analyze SKIP_LOCKED tp;
+1: rollback;
+
+1:drop table tp;
+1:drop table tp1;
