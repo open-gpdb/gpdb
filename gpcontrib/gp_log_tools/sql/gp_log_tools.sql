d6ef025d7a4024793bba6457f939179440981762
Vladimir Rachkin <vova@kpnn.ru>
2025-07-10 14:01:38 +0300

    Fix test for gp_log_tools

--- gpcontrib/gp_log_tools/sql/gp_log_tools.sql
+++ gpcontrib/gp_log_tools/sql/gp_log_tools.sql
@@ -1,15 +1,5 @@
--- start_matchsubs
--- m/^\s+logtime.*/
--- s/^\s+logtime.*/ log_table/
--- m/^\(\d+ row[s]?\)/
--- s/^\(\d+ row[s]?\)/(XXX rows)/
--- end_matchsubs
--- start_matchignore
--- m/^([\s\S]+\|){10}/
--- end_matchignore
-
 CREATE EXTENSION gp_log_tools;
 
-SELECT * FROM gp_master_logs;
+SELECT count(*) != 0 as is_logs_selected FROM gp_master_logs;
 
 DROP EXTENSION gp_log_tools;
