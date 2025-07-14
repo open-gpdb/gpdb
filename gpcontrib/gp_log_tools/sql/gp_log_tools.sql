7fef158f84ca295fa0b490e57ead2a6bd36b42da
Vladimir Rachkin <vova@kpnn.ru>
2025-07-14 12:22:57 +0300

    Add test for empty user test

--- gpcontrib/gp_log_tools/sql/gp_log_tools.sql
+++ gpcontrib/gp_log_tools/sql/gp_log_tools.sql
@@ -4,4 +4,16 @@ SHOW ycmdb.yc_allow_copy_from_logs;
 
 SELECT count(*) != 0 as is_logs_selected FROM gp_master_logs;
 
+CREATE USER test_user1;
+
+GRANT SELECT ON gp_master_logs TO PUBLIC;
+
+SET SESSION AUTHORIZATION test_user1;
+
+SELECT count(*) != 0 as is_logs_selected FROM gp_master_logs;
+
+\c -
+
+DROP USER test_user1;
+
 DROP EXTENSION gp_log_tools;
