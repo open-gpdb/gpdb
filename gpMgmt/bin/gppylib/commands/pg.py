fa526e228ee1b96394626c13e41673b92ebce099
reshke kirill <reshke@double.cloud>
2025-09-15 08:33:38 +0000

    add debug

--- gpMgmt/bin/gppylib/commands/pg.py
+++ gpMgmt/bin/gppylib/commands/pg.py
@@ -300,7 +300,7 @@ class PgRewind(Command):
         # be started up normally as a mirror for WAL replication catch up.
         rewind_cmd = '[ -f %s/recovery.conf ] || PGOPTIONS="-c gp_session_role=utility" $GPHOME/bin/pg_rewind ' \
                      '--write-recovery-conf --slot="internal_wal_replication_slot" --source-server="%s" ' \
-                     '--target-pgdata=%s --progress --restore-target-wal --source-segment-id=%s' % (target_datadir, source_server, target_datadir, target_contend_id)
+                     '--target-pgdata=%s --progress --debug --restore-target-wal --source-segment-id=%s' % (target_datadir, source_server, target_datadir, target_contend_id)
 
         # pg_rewind prints progress updates to stdout, but it also prints
         # errors relating to relevant failures(like it will not rewind due to
