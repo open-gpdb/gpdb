8016d7d151974cc3fc2159910941295257ccab27
NJrslv <jaroslav.questions@gmail.com>
2025-09-22 16:58:58 +0300

    add test

--- src/test/regress/sql/event_trigger.sql
+++ src/test/regress/sql/event_trigger.sql
@@ -261,3 +261,12 @@ alter table rewriteme alter column foo type numeric(12,4);
 drop table rewriteme;
 drop event trigger no_rewrite_allowed;
 drop function test_evtrig_no_rewrite();
+
+-- test that alter table state object is not clobbered, shouldn't crash
+create event trigger regress_event_trigger_alter_tbl on ddl_command_end 
+	execute procedure test_event_trigger();
+create table part_tbl(r int) partition by range(r) (start(1) end(10) every(1));
+alter table part_tbl add partition start(11) end(20);
+
+drop table part_tbl;
+drop event trigger regress_event_trigger_alter_tbl;
