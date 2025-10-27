169143d1b8102a68d6e18295139fd6cd32a42ed8
reshke <reshkekirill@gmail.com>
2025-10-27 10:41:39 +0500

    Allow extension to overwrite about-to-be created TOAST tablespace (#278)
    * Allow extension to overwrite about-to-be created TOAST tablespace
    
    * remove abi change

--- src/backend/commands/tablecmds.c
+++ src/backend/commands/tablecmds.c
@@ -152,6 +152,7 @@ typedef struct OnCommitItem
 
 static List *on_commits = NIL;
 
+Oid newTOASTTableSpace = InvalidOid;
 
 /*
  * State information for ALTER TABLE
@@ -197,6 +198,7 @@ typedef struct AlteredTableInfo
 	bool		dist_opfamily_changed; /* T if changing datatype of distribution key column and new opclass is in different opfamily than old one */
 	Oid			new_opclass;		/* new opclass, if changing a distribution key column */
 	Oid			newTableSpace;	/* new tablespace; 0 means no change */
+	Oid			newTOASTTableSpace;	/* new TOAST tablespace; 0 means no change */
 	Oid			exchange_relid;	/* for EXCHANGE, the exchanged in rel */
 	/* Objects to rebuild after completing ALTER TYPE operations */
 	List	   *changedConstraintOids;	/* OIDs of constraints to rebuild */
@@ -4350,6 +4352,11 @@ ATPrepCmd(List **wqueue, Relation rel, AlterTableCmd *cmd,
 	/* Find or create work queue entry for this table */
 	tab = ATGetQueueEntry(wqueue, rel);
 
+	tab->newTOASTTableSpace = newTOASTTableSpace;
+
+	/* Reset it */
+	newTOASTTableSpace = InvalidOid;
+
 	/*
 	 * Copy the original subcommand for each table.  This avoids conflicts
 	 * when different child tables need to make different parse
@@ -5981,6 +5988,7 @@ ATRewriteTables(AlterTableStmt *parsetree, List **wqueue, LOCKMODE lockmode)
 		Relation	OldHeap;
 		Oid			newTableSpace;
 		Oid 		oldTableSpace;
+		Oid			newTOASTTableSpace;
 		bool		hasIndexes;
 
 		/* We will lock the table iff we decide to actually rewrite it */
@@ -5993,6 +6001,7 @@ ATRewriteTables(AlterTableStmt *parsetree, List **wqueue, LOCKMODE lockmode)
 
 		oldTableSpace = OldHeap->rd_rel->reltablespace;
 		newTableSpace = tab->newTableSpace ? tab->newTableSpace : oldTableSpace;
+		newTOASTTableSpace = tab->newTOASTTableSpace ? tab->newTOASTTableSpace : newTableSpace;
 		relstorage    = OldHeap->rd_rel->relstorage;
 		{
 			List	   *indexIds;
@@ -6081,7 +6090,7 @@ ATRewriteTables(AlterTableStmt *parsetree, List **wqueue, LOCKMODE lockmode)
 			 * unlogged anyway.
 			 */
 			/* Create transient table that will receive the modified data */
-			OIDNewHeap = make_new_heap(tab->relid, newTableSpace, false,
+			OIDNewHeap = make_new_heap(tab->relid, newTableSpace, newTOASTTableSpace, false,
 									   lockmode, hasIndexes, false);
 
 			/*
