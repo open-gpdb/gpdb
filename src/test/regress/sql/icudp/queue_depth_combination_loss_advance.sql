2c212feda5ed414db65b8f0f39ab57d97def2edf
NJrslv <jaroslav.questions@gmail.com>
2025-11-26 16:38:47 +0300

    port cloudberry: Optimize motion timeout transmission #1365

--- src/test/regress/sql/icudp/queue_depth_combination_loss_advance.sql
+++ src/test/regress/sql/icudp/queue_depth_combination_loss_advance.sql
@@ -1,9 +1,9 @@
 -- 
 -- @description Interconncet flow control test case: combination guc value
--- @created 2012-11-13
--- @modified 2012-11-13
--- @tags executor
--- @gpdb_version [4.2.3.0,main]
+-- @created 2025-09-12
+
+-- Set mode
+SET gp_interconnect_fc_method = "loss_advance";
 
 -- Create a table
 CREATE TEMP TABLE small_table(dkey INT, jkey INT, rval REAL, tval TEXT default 'abcdefghijklmnopqrstuvwxyz') DISTRIBUTED BY (dkey);
@@ -13,64 +13,6 @@ INSERT INTO small_table VALUES(generate_series(1, 5000), generate_series(5001, 1
 
 -- Functional tests
 -- Skew with gather+redistribute
-SET gp_interconnect_fc_method = "capacity";
-SHOW gp_interconnect_fc_method;
-
-SELECT ROUND(foo.rval * foo.rval)::INT % 30 AS rval2, COUNT(*) AS count, SUM(length(foo.tval)) AS sum_len_tval
-  FROM (SELECT 5001 AS jkey, rval, tval FROM small_table ORDER BY dkey LIMIT 3000) foo
-    JOIN small_table USING(jkey)
-  GROUP BY rval2
-  ORDER BY rval2;
-
--- Set GUC values
-SET gp_interconnect_snd_queue_depth = 1;
-SET gp_interconnect_queue_depth = 1;
-SELECT ROUND(foo.rval * foo.rval)::INT % 30 AS rval2, COUNT(*) AS count, SUM(length(foo.tval)) AS sum_len_tval
-  FROM (SELECT 5001 AS jkey, rval, tval FROM small_table ORDER BY dkey LIMIT 3000) foo
-    JOIN small_table USING(jkey)
-  GROUP BY rval2
-  ORDER BY rval2;
-
--- Set GUC values
-SET gp_interconnect_snd_queue_depth = 4096;
-SET gp_interconnect_queue_depth = 4096;
-SELECT ROUND(foo.rval * foo.rval)::INT % 30 AS rval2, COUNT(*) AS count, SUM(length(foo.tval)) AS sum_len_tval
-  FROM (SELECT 5001 AS jkey, rval, tval FROM small_table ORDER BY dkey LIMIT 3000) foo
-    JOIN small_table USING(jkey)
-  GROUP BY rval2
-  ORDER BY rval2;
-
--- Set GUC values
-SET gp_interconnect_snd_queue_depth = 1;
-SET gp_interconnect_queue_depth = 4096;
-SELECT ROUND(foo.rval * foo.rval)::INT % 30 AS rval2, COUNT(*) AS count, SUM(length(foo.tval)) AS sum_len_tval
-  FROM (SELECT 5001 AS jkey, rval, tval FROM small_table ORDER BY dkey LIMIT 3000) foo
-    JOIN small_table USING(jkey)
-  GROUP BY rval2
-  ORDER BY rval2;
-
--- Set GUC values
-SET gp_interconnect_snd_queue_depth = 4096;
-SET gp_interconnect_queue_depth = 1;
-SELECT ROUND(foo.rval * foo.rval)::INT % 30 AS rval2, COUNT(*) AS count, SUM(length(foo.tval)) AS sum_len_tval
-  FROM (SELECT 5001 AS jkey, rval, tval FROM small_table ORDER BY dkey LIMIT 3000) foo
-    JOIN small_table USING(jkey)
-  GROUP BY rval2
-  ORDER BY rval2;
-
--- Set GUC values
-SET gp_interconnect_snd_queue_depth = 1024;
-SET gp_interconnect_queue_depth = 1024;
-SELECT ROUND(foo.rval * foo.rval)::INT % 30 AS rval2, COUNT(*) AS count, SUM(length(foo.tval)) AS sum_len_tval
-  FROM (SELECT 5001 AS jkey, rval, tval FROM small_table ORDER BY dkey LIMIT 3000) foo
-    JOIN small_table USING(jkey)
-  GROUP BY rval2
-  ORDER BY rval2;
-
--- Skew with gather+redistribute
-SET gp_interconnect_fc_method = "loss_advance";
-SHOW gp_interconnect_fc_method;
-
 SELECT ROUND(foo.rval * foo.rval)::INT % 30 AS rval2, COUNT(*) AS count, SUM(length(foo.tval)) AS sum_len_tval
   FROM (SELECT 5001 AS jkey, rval, tval FROM small_table ORDER BY dkey LIMIT 3000) foo
     JOIN small_table USING(jkey)
