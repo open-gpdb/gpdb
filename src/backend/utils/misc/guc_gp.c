00c7301aeeb60743f46de2fdedefdede135bcd16
NJrslv <108277031+NJrslv@users.noreply.github.com>
2025-12-08 14:29:11 +0300

    port Optimize motion timeout transmission #1365 from cloudberry (#309)
    Ported from apache/cloudberry#1365.
    
    Code was changed to use shared MotionConn object between TPC/UDP interconnect as the MotionConn was refactored into MotionConnUDP and MotionConnTCP in cloudberry.
    
    On 20TB TPC-DS runs, retransmits drop and system stays more stable when concurrency increases.

--- src/backend/utils/misc/guc_gp.c
+++ src/backend/utils/misc/guc_gp.c
@@ -560,6 +560,8 @@ static const struct config_enum_entry gp_autostats_modes[] = {
 static const struct config_enum_entry gp_interconnect_fc_methods[] = {
 	{"loss", INTERCONNECT_FC_METHOD_LOSS},
 	{"capacity", INTERCONNECT_FC_METHOD_CAPACITY},
+	{"loss_advance", INTERCONNECT_FC_METHOD_LOSS_ADVANCE},
+	{"loss_timer", INTERCONNECT_FC_METHOD_LOSS_TIMER},
 	{NULL, 0}
 };
 
@@ -3848,6 +3850,16 @@ struct config_int ConfigureNamesInt_gp[] =
 		},
 		&Gp_interconnect_snd_queue_depth,
 		2, 1, 4096,
+		NULL, NULL, NULL
+	},
+
+	{
+		{"gp_interconnect_mem_size", PGC_USERSET, GP_ARRAY_TUNING,
+			gettext_noop("Sets the maximum size(in MB) of the send/recv queue memory for all connections in the UDP interconnect"),
+			NULL
+		},
+		&Gp_interconnect_mem_size,
+		10, 1, 1024,
 		NULL, NULL, NULL
 	},
 
