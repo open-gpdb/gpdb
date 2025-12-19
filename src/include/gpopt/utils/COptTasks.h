0fab219fd687bec71b7ce0b05c0e99516a7ffa6f
NJrslv <108277031+NJrslv@users.noreply.github.com>
2025-12-19 14:00:26 +0300

    Fix std::terminate() caused by uncaught exception in ORCA (#312)
    ### What does this commit do?
    This PR fixes a crash, `std::terminate()`, caused by an exception occurring in the catch block of `GPOPTOptimizedPlan()` during `gpopt_context.CloneErrorMsg()`.
    
    When `CloneErrorMsg` fails (typically due to OOM), it throws a `gpdb` error, causing `std::terminate()`.
    
    Postgres has graceful error handling for errors that occur while processing other errors - even during OOM, the `ErrorContext` has reserved space to preserve error messages. 
    
    So rethrow the error to postgres handler.
    
    ### Test Plan
    Added a new test to the regression `gporca_fault` test.
     
    I added a query that errors out during optimization in orca and as the result falls into `catch` block where another exception in `CloneErrorMsg` is thrown via inject fault. 
    
    Previously it resulted in the `std::terminate()` call & process crashing. Now catch the exception and let the postgres handle it with reserved buffer to preserve error message.
    
    ### Impact
    Instead of 
    ```sql
    server closed the connection unexpectedly
            This probably means the server terminated abnormally
            before or while processing the request.
    ```
    postgres reports an error (in case of OOM we will see it, not std::terminate with exiting processes):
    ```sql
    ERROR:  ...
    ```

--- src/include/gpopt/utils/COptTasks.h
+++ src/include/gpopt/utils/COptTasks.h
@@ -106,7 +106,7 @@ struct SOptContext
 	void Free(EPin input, EPin epinOutput);
 
 	// Clone the error message in given context.
-	CHAR *CloneErrorMsg(struct MemoryContextData *context);
+	CHAR *CloneErrorMsg(struct MemoryContextData *context, BOOL *clone_failed);
 
 	// casting function
 	static SOptContext *Cast(void *ptr);
