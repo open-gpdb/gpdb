0b10183bc49600db6ef84babcf9eac7d488b14d0
Andrey Sokolov <sokolov.andrey.yurevich@gmail.com>
2025-12-24 17:59:53 +0300

    Fix AOCO tables expansion
    When a column was added to AOCO after adding new segments to cluster and before
    the table expansion, then an error happened on gpexpand calling pg_relation_size
    because pg_aocsseg.vpinfo contains invalid value. The table was not expanded in
    this case.
    
    A new segment is created based on master using pg_basebackup. AOCO tables on
    master are empty, but their pg_aocsseg table contains rows where tupcount > 0,
    but all EOFs in vpinfo are 0. These rows are copied to new segments. When
    a column is added then the vpinfo column is not updated on the new segment,
    because the aocs_addcol_emptyvpe function changes vpinfo when tupcount is 0
    and AOCSFileSegInfoAddVpe is called from other functions when EOFs in vpinfo are
    greater than zero.
    
    GPDB 7 is not affected, because pg_aocsseg on master does not contains rows.
    
    We cannot change data in pg_aocsseg on master, but we can truncate AOCO tables
    on new segments.

---
+++ src/test/regress/sql/bfv_gpexpand.sql
@@ -0,0 +1,94 @@
+-- Check that it is possible to add columns to AOCO tables in the period
+-- between adding new segments to a cluster and expanding the tables.
+
+-- start_ignore
+drop table if exists t, tp, tsp;
+\c postgres
+drop schema if exists gpexpand, gpshrink cascade;
+\c regression
+\! rm -rf /tmp/datadirs/dbfast_mirror4/ /tmp/datadirs/dbfast4/
+-- end_ignore
+
+-- Create table without partitions
+create table t (i int, j int)
+with (appendonly = true, orientation = column)
+distributed by (i);
+
+-- Create table with partitions
+create table tp (i int, j int)
+with (appendonly = true, orientation = column)
+distributed by (i)
+partition by range (j) (partition p start (0) end (2) every(1));
+
+-- Create table with subpartitions
+create table tsp (i int, j int, k int)
+with (appendonly = true, orientation = column)
+distributed by (i)
+partition by range (j)
+  subpartition by range (k)
+  subpartition template (subpartition sp start (0) end (2) every(1))
+(partition p start (0) end (2) every(1));
+
+-- Add tuples to the tables to fill their pg_aocsseg-s on master
+insert into t (i, j) values (1, 1);
+insert into tp (i, j) values (1, 1);
+insert into tsp (i, j, k) values (1, 1, 1);
+
+-- Check that pg_aocsseg-s contain tuples
+select string_agg('table ' || segrelid::regclass::text, ' union all ')
+         || ';' show_pg_aocssegs
+  from pg_appendonly
+ where relid in ('t'::regclass,
+                 'tp_1_prt_p_2'::regclass,
+                 'tsp_1_prt_p_2_2_prt_sp_2'::regclass) \gset
+
+:show_pg_aocssegs
+
+-- Add new segments
+\! echo "localhost|localhost|7008|/tmp/datadirs/dbfast4/demoDataDir3|9|3|p" > /tmp/testexpand
+\! echo "localhost|localhost|7009|/tmp/datadirs/dbfast_mirror4/demoDataDir3|10|3|m" >> /tmp/testexpand
+
+-- start_ignore
+\! gpexpand -i /tmp/testexpand
+-- end_ignore
+
+-- Add columns to the tables
+alter table t add b boolean;
+alter table tp add b boolean;
+alter table tsp add b boolean;
+
+-- Expand tables, no error must happen
+\! gpexpand | grep ERROR | wc -l
+
+-- Check that the tables are expanded
+select numsegments
+  from gp_distribution_policy
+ where localoid in ('t'::regclass,
+                    'tp_1_prt_p_2'::regclass,
+                    'tsp_1_prt_p_2_2_prt_sp_2'::regclass);
+
+\c postgres
+select status
+  from gpexpand.status_detail
+ where dbname = 'regression'
+   and fq_name in ('public.t',
+                   'public.tp_1_prt_p_2',
+                   'public.tsp_1_prt_p_2_2_prt_sp_2');
+\c regression
+
+
+-- Cleanup
+-- start_ignore
+\! gpshrink -i /tmp/testexpand
+\! gpshrink -i /tmp/testexpand
+-- end_ignore
+
+-- Check that cluster is shrunk
+select count(*) from gp_segment_configuration;
+
+drop table t, tp, tsp;
+\c postgres
+drop schema gpexpand, gpshrink cascade;
+\c regression
+
+\! rm -rf /tmp/datadirs/dbfast_mirror4/ /tmp/datadirs/dbfast4/ /tmp/testexpand
