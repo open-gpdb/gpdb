266347bf8c594134b5cc7c91a774464e271e0b62
Andrey Sokolov <sokolov.andrey.yurevich@gmail.com>
2026-03-25 13:59:56 +0300

    Add the access_log extension (#361)
    When we need to reduce the used amount of disk space, we should drop some tables
    or partitions. In this case, we need to find out tables or partitions which are
    not read from. This extension logs when and which user initializes Seq Scan on
    which partition or table. The log file name is pg_log/access.log.
    Add a new hook - init_scan_hook which is called from InitScanRelation.

---
+++ gpcontrib/access_log/access_log.c
@@ -0,0 +1,94 @@
+#include "postgres.h"
+
+#include "catalog/pg_namespace.h"
+#include "executor/nodeSeqscan.h"
+#include "libpq/auth.h"
+#include "utils/syscache.h"
+
+
+PG_MODULE_MAGIC;
+
+#define LOG_FILE_NAME "pg_log/access.log"
+
+void _PG_init(void);
+
+
+static init_scan_hook_type next_init_scan_hook = NULL;
+
+static void
+write_to_log(const char* str)
+{
+	/*
+	 * Synchronization is not necessary because `man write` says:
+	 * "If the file was open(2)ed with O_APPEND, the file offset is first set to
+	 * the end of the file before writing. The adjustment of the file offset and
+	 * the write operation are performed as an atomic step."
+	 */
+	int f = OpenTransientFile(LOG_FILE_NAME, O_WRONLY | O_APPEND | O_CREAT,
+							  S_IRUSR | S_IWUSR);
+	if (f < 0 )
+	{
+		ereport(WARNING, (errcode_for_file_access(),
+				errmsg("could not open file " LOG_FILE_NAME ": %m")));
+		return;
+	}
+	write(f, str, strlen(str));
+	CloseTransientFile(f);
+}
+
+static void
+access_log_init_scan_hook(Relation currentRelation)
+{
+	char		buf[512];
+	HeapTuple	tp;
+	struct timeval	tv;
+	pg_time_t	stamp_time;
+
+	gettimeofday(&tv, NULL);
+	stamp_time = (pg_time_t) tv.tv_sec;
+	pg_strftime(buf, sizeof(buf),
+			"%Y-%m-%d %H:%M:%S        %Z,",
+			pg_localtime(&stamp_time, log_timezone));
+
+	/* paste milliseconds into place */
+	sprintf(buf + 19, ".%06d", (int) (tv.tv_usec));
+	/* restore space which has been rewritten by sprintf with \0 */
+	buf[19 + 1 + 6] = ' ';
+
+	if (MyProcPort != NULL && MyProcPort->user_name != NULL)
+		strlcat(buf, MyProcPort->user_name, sizeof(buf));
+	strlcat(buf, ",", sizeof(buf));
+
+	tp = SearchSysCache1(NAMESPACEOID,
+					ObjectIdGetDatum(currentRelation->rd_rel->relnamespace));
+	if (HeapTupleIsValid(tp))
+	{
+		Form_pg_namespace nsptup = (Form_pg_namespace) GETSTRUCT(tp);
+		strlcat(buf, NameStr(nsptup->nspname), sizeof(buf));
+		strlcat(buf, ".", sizeof(buf));
+		ReleaseSysCache(tp);
+	}
+
+	strlcat(buf, NameStr(currentRelation->rd_rel->relname), sizeof(buf));
+	strlcat(buf, "\n", sizeof(buf));
+
+	write_to_log(buf);
+
+	if (next_init_scan_hook)
+		next_init_scan_hook(currentRelation);
+}
+
+void
+_PG_init(void)
+{
+	/* Be sure we do initialization only once */
+	static bool inited = false;
+
+	if (inited)
+		return;
+
+	next_init_scan_hook = init_scan_hook;
+	init_scan_hook = access_log_init_scan_hook;
+
+	inited = true;
+}
