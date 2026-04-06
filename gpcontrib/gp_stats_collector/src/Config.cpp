a37b3875d736c222053cedbe4b9c76ffe4ed4e6e
NJrslv <jaroslav.questions@gmail.com>
2026-04-06 15:22:38 +0300

    [gp_stats_collector] Rename yagp_hooks_collector to gp_stats_collector

---
+++ gpcontrib/gp_stats_collector/src/Config.cpp
@@ -0,0 +1,150 @@
+#include "Config.h"
+#include "memory/gpdbwrappers.h"
+#include <limits.h>
+#include <memory>
+#include <string>
+#include <unordered_set>
+
+extern "C" {
+#include "postgres.h"
+#include "utils/guc.h"
+}
+
+static char *guc_uds_path = nullptr;
+static bool guc_enable_analyze = true;
+static bool guc_enable_cdbstats = true;
+static bool guc_enable_collector = true;
+static bool guc_report_nested_queries = true;
+static char *guc_ignored_users = nullptr;
+static int guc_max_text_size = 1 << 20;  // in bytes (1MB)
+static int guc_max_plan_size = 1024;     // in KB
+static int guc_min_analyze_time = 10000; // in ms
+static int guc_logging_mode = LOG_MODE_UDS;
+static bool guc_enable_utility = false;
+
+static const struct config_enum_entry logging_mode_options[] = {
+    {"uds", LOG_MODE_UDS, false /* hidden */},
+    {"tbl", LOG_MODE_TBL, false},
+    {NULL, 0, false}};
+
+static bool ignored_users_guc_dirty = false;
+
+static void assign_ignored_users_hook(const char *, void *) {
+  ignored_users_guc_dirty = true;
+}
+
+void Config::init_gucs() {
+  DefineCustomStringVariable(
+      "gpsc.uds_path", "Sets filesystem path of the agent socket", 0LL,
+      &guc_uds_path, "/tmp/gpsc_agent.sock", PGC_SUSET,
+      GUC_NOT_IN_SAMPLE | GUC_GPDB_NEED_SYNC, 0LL, 0LL, 0LL);
+
+  DefineCustomBoolVariable(
+      "gpsc.enable", "Enable metrics collector", 0LL, &guc_enable_collector,
+      true, PGC_SUSET, GUC_NOT_IN_SAMPLE | GUC_GPDB_NEED_SYNC, 0LL, 0LL, 0LL);
+
+  DefineCustomBoolVariable(
+      "gpsc.enable_analyze", "Collect analyze metrics in gpsc", 0LL,
+      &guc_enable_analyze, true, PGC_SUSET,
+      GUC_NOT_IN_SAMPLE | GUC_GPDB_NEED_SYNC, 0LL, 0LL, 0LL);
+
+  DefineCustomBoolVariable(
+      "gpsc.enable_cdbstats", "Collect CDB metrics in gpsc", 0LL,
+      &guc_enable_cdbstats, true, PGC_SUSET,
+      GUC_NOT_IN_SAMPLE | GUC_GPDB_NEED_SYNC, 0LL, 0LL, 0LL);
+
+  DefineCustomBoolVariable(
+      "gpsc.report_nested_queries", "Collect stats on nested queries", 0LL,
+      &guc_report_nested_queries, true, PGC_USERSET,
+      GUC_NOT_IN_SAMPLE | GUC_GPDB_NEED_SYNC, 0LL, 0LL, 0LL);
+
+  DefineCustomStringVariable("gpsc.ignored_users_list",
+                             "Make gpsc ignore queries issued by given users",
+                             0LL, &guc_ignored_users,
+                             "gpadmin,repl,gpperfmon,monitor", PGC_SUSET,
+                             GUC_NOT_IN_SAMPLE | GUC_GPDB_NEED_SYNC, 0LL,
+                             assign_ignored_users_hook, 0LL);
+
+  DefineCustomIntVariable(
+      "gpsc.max_text_size",
+      "Make gpsc trim query texts longer than configured size in bytes", NULL,
+      &guc_max_text_size, 1 << 20 /* 1MB */, 0, INT_MAX, PGC_SUSET,
+      GUC_NOT_IN_SAMPLE | GUC_GPDB_NEED_SYNC, NULL, NULL, NULL);
+
+  DefineCustomIntVariable(
+      "gpsc.max_plan_size",
+      "Make gpsc trim plan longer than configured size", NULL,
+      &guc_max_plan_size, 1024, 0, INT_MAX / 1024, PGC_SUSET,
+      GUC_NOT_IN_SAMPLE | GUC_GPDB_NEED_SYNC | GUC_UNIT_KB, NULL, NULL, NULL);
+
+  DefineCustomIntVariable(
+      "gpsc.min_analyze_time",
+      "Sets the minimum execution time above which plans will be logged.",
+      "Zero prints all plans. -1 turns this feature off.",
+      &guc_min_analyze_time, 10000, -1, INT_MAX, PGC_USERSET,
+      GUC_NOT_IN_SAMPLE | GUC_GPDB_NEED_SYNC | GUC_UNIT_MS, NULL, NULL, NULL);
+
+  DefineCustomEnumVariable(
+      "gpsc.logging_mode", "Logging mode: UDS or PG Table", NULL,
+      &guc_logging_mode, LOG_MODE_UDS, logging_mode_options, PGC_SUSET,
+      GUC_NOT_IN_SAMPLE | GUC_GPDB_NEED_SYNC | GUC_SUPERUSER_ONLY, NULL, NULL,
+      NULL);
+
+  DefineCustomBoolVariable(
+      "gpsc.enable_utility", "Collect utility statement stats", NULL,
+      &guc_enable_utility, false, PGC_USERSET,
+      GUC_NOT_IN_SAMPLE | GUC_GPDB_NEED_SYNC, NULL, NULL, NULL);
+}
+
+void Config::update_ignored_users(const char *new_guc_ignored_users) {
+  auto new_ignored_users_set = std::make_unique<IgnoredUsers>();
+  if (new_guc_ignored_users != nullptr && new_guc_ignored_users[0] != '\0') {
+    /* Need a modifiable copy of string */
+    char *rawstring = gpdb::pstrdup(new_guc_ignored_users);
+    List *elemlist;
+    ListCell *l;
+
+    /* Parse string into list of identifiers */
+    if (!gpdb::split_identifier_string(rawstring, ',', &elemlist)) {
+      /* syntax error in list */
+      gpdb::pfree(rawstring);
+      gpdb::list_free(elemlist);
+      ereport(
+          LOG,
+          (errcode(ERRCODE_SYNTAX_ERROR),
+           errmsg(
+               "invalid list syntax in parameter gpsc.ignored_users_list")));
+      return;
+    }
+    foreach (l, elemlist) {
+      new_ignored_users_set->insert((char *)lfirst(l));
+    }
+    gpdb::pfree(rawstring);
+    gpdb::list_free(elemlist);
+  }
+  ignored_users_ = std::move(new_ignored_users_set);
+}
+
+bool Config::filter_user(const std::string &username) const {
+  if (!ignored_users_) {
+    return true;
+  }
+  return ignored_users_->find(username) != ignored_users_->end();
+}
+
+void Config::sync() {
+  if (ignored_users_guc_dirty) {
+    update_ignored_users(guc_ignored_users);
+    ignored_users_guc_dirty = false;
+  }
+  uds_path_ = guc_uds_path;
+  enable_analyze_ = guc_enable_analyze;
+  enable_cdbstats_ = guc_enable_cdbstats;
+  enable_collector_ = guc_enable_collector;
+  enable_utility_ = guc_enable_utility;
+  report_nested_queries_ = guc_report_nested_queries;
+  max_text_size_ = guc_max_text_size;
+  max_plan_size_ = guc_max_plan_size;
+  min_analyze_time_ = guc_min_analyze_time;
+  logging_mode_ = guc_logging_mode;
+}
---
+++ gpcontrib/gp_stats_collector/src/GpscStat.cpp
@@ -0,0 +1,91 @@
+#include "GpscStat.h"
+
+#include <algorithm>
+
+extern "C" {
+#include "postgres.h"
+#include "miscadmin.h"
+#include "storage/ipc.h"
+#include "storage/lwlock.h"
+#include "storage/shmem.h"
+#include "storage/spin.h"
+}
+
+namespace {
+struct ProtectedData {
+  slock_t mutex;
+  GpscStat::Data data;
+};
+shmem_startup_hook_type prev_shmem_startup_hook = NULL;
+ProtectedData *data = nullptr;
+
+void gpsc_shmem_startup() {
+  if (prev_shmem_startup_hook)
+    prev_shmem_startup_hook();
+  LWLockAcquire(AddinShmemInitLock, LW_EXCLUSIVE);
+  bool found;
+  data = reinterpret_cast<ProtectedData *>(
+      ShmemInitStruct("gpsc_stat_messages", sizeof(ProtectedData), &found));
+  if (!found) {
+    SpinLockInit(&data->mutex);
+    data->data = GpscStat::Data();
+  }
+  LWLockRelease(AddinShmemInitLock);
+}
+
+class LockGuard {
+public:
+  LockGuard(slock_t *mutex) : mutex_(mutex) { SpinLockAcquire(mutex_); }
+  ~LockGuard() { SpinLockRelease(mutex_); }
+
+private:
+  slock_t *mutex_;
+};
+} // namespace
+
+void GpscStat::init() {
+  if (!process_shared_preload_libraries_in_progress)
+    return;
+  RequestAddinShmemSpace(sizeof(ProtectedData));
+  prev_shmem_startup_hook = shmem_startup_hook;
+  shmem_startup_hook = gpsc_shmem_startup;
+}
+
+void GpscStat::deinit() { shmem_startup_hook = prev_shmem_startup_hook; }
+
+void GpscStat::reset() {
+  LockGuard lg(&data->mutex);
+  data->data = GpscStat::Data();
+}
+
+void GpscStat::report_send(int32_t msg_size) {
+  LockGuard lg(&data->mutex);
+  data->data.total++;
+  data->data.max_message_size = std::max(msg_size, data->data.max_message_size);
+}
+
+void GpscStat::report_bad_connection() {
+  LockGuard lg(&data->mutex);
+  data->data.total++;
+  data->data.failed_connects++;
+}
+
+void GpscStat::report_bad_send(int32_t msg_size) {
+  LockGuard lg(&data->mutex);
+  data->data.total++;
+  data->data.failed_sends++;
+  data->data.max_message_size = std::max(msg_size, data->data.max_message_size);
+}
+
+void GpscStat::report_error() {
+  LockGuard lg(&data->mutex);
+  data->data.total++;
+  data->data.failed_other++;
+}
+
+GpscStat::Data GpscStat::get_stats() {
+  LockGuard lg(&data->mutex);
+  return data->data;
+}
+
+bool GpscStat::loaded() { return data != nullptr; }
