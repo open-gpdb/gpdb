open-gpdb/gpdb

commit d31f362250105e456961c2c9249693e42e67eca9

Convert datatype cast functions for "soft" error handling (#23)
* Convert cast functions for soft error handling

* Remove commented code

contrib/hstore/hstore_io.c
@@ -17,6 +17,7 @@
 #include "utils/lsyscache.h"
 #include "utils/memutils.h"
 #include "utils/typcache.h"
+#include "nodes/nodes.h"
 
 #include "hstore.h"
 
@@ -58,7 +59,7 @@ do { \
 #define GV_WAITESCESCIN 4
 
 static bool
-get_val(HSParser *state, bool ignoreeq, bool *escaped)
+get_val(HSParser *state, bool ignoreeq, bool *escaped, bool *result, Node *escontext)
 {
 	int			st = GV_WAITVAL;
 
@@ -77,11 +78,14 @@ get_val(HSParser *state, bool ignoreeq, bool *escaped)
 			}
 			else if (*(state->ptr) == '\0')
 			{
-				return false;
+				*result = false;
+				return true;
 			}
 			else if (*(state->ptr) == '=' && !ignoreeq)
 			{
-				elog(ERROR, "Syntax error near '%c' at position %d", *(state->ptr), (int32) (state->ptr - state->begin));
+				ereturn(escontext, false, 
+					(errcode(ERRCODE_INVALID_TEXT_REPRESENTATION),
+					errmsg("Syntax error near '%c' at position %d", *(state->ptr), (int32) (state->ptr - state->begin))));
 			}
 			else if (*(state->ptr) == '\\')
 			{
@@ -103,20 +107,24 @@ get_val(HSParser *state, bool ignoreeq, bool *escaped)
 			else if (*(state->ptr) == '=' && !ignoreeq)
 			{
 				state->ptr--;
+				*result = true;
 				return true;
 			}
 			else if (*(state->ptr) == ',' && ignoreeq)
 			{
 				state->ptr--;
+				*result = true;
 				return true;
 			}
 			else if (isspace((unsigned char) *(state->ptr)))
 			{
+				*result = true;
 				return true;
 			}
 			else if (*(state->ptr) == '\0')
 			{
 				state->ptr--;
+				*result = true;
 				return true;
 			}
 			else
@@ -134,11 +142,14 @@ get_val(HSParser *state, bool ignoreeq, bool *escaped)
 			}
 			else if (*(state->ptr) == '"')
 			{
+				*result = true;
 				return true;
 			}
 			else if (*(state->ptr) == '\0')
 			{
-				elog(ERROR, "Unexpected end of string");
+				ereturn(escontext, false, 
+					(errcode(ERRCODE_INVALID_TEXT_REPRESENTATION),
+					errmsg("Unexpected end of string")));
 			}
 			else
 			{
@@ -150,7 +161,9 @@ get_val(HSParser *state, bool ignoreeq, bool *escaped)
 		else if (st == GV_WAITESCIN)
 		{
 			if (*(state->ptr) == '\0')
-				elog(ERROR, "Unexpected end of string");
+				ereturn(escontext, false, 
+					(errcode(ERRCODE_INVALID_TEXT_REPRESENTATION),
+					errmsg("Unexpected end of string")));
 			RESIZEPRSBUF;
 			*(state->cur) = *(state->ptr);
 			state->cur++;
@@ -159,14 +172,18 @@ get_val(HSParser *state, bool ignoreeq, bool *escaped)
 		else if (st == GV_WAITESCESCIN)
 		{
 			if (*(state->ptr) == '\0')
-				elog(ERROR, "Unexpected end of string");
+				ereturn(escontext, false, 
+					(errcode(ERRCODE_INVALID_TEXT_REPRESENTATION),
+					errmsg("Unexpected end of string")));
 			RESIZEPRSBUF;
 			*(state->cur) = *(state->ptr);
 			state->cur++;
 			st = GV_INESCVAL;
 		}
 		else
-			elog(ERROR, "Unknown state %d at position line %d in file '%s'", st, __LINE__, __FILE__);
+			ereturn(escontext, false, 
+					(errcode(ERRCODE_INVALID_TEXT_REPRESENTATION),
+					errmsg("Unknown state %d at position line %d in file '%s'", st, __LINE__, __FILE__)));
 
 		state->ptr++;
 	}
@@ -179,11 +196,12 @@ get_val(HSParser *state, bool ignoreeq, bool *escaped)
 #define WDEL	4
 
 
-static void
-parse_hstore(HSParser *state)
+static bool
+parse_hstore(HSParser *state, Node *escontext)
 {
 	int			st = WKEY;
 	bool		escaped = false;
+	bool		is_got_val = false;
 
 	state->plen = 16;
 	state->pairs = (Pairs *) palloc(sizeof(Pairs) * state->plen);
@@ -195,15 +213,16 @@ parse_hstore(HSParser *state)
 	{
 		if (st == WKEY)
 		{
-			if (!get_val(state, false, &escaped))
-				return;
+			safe_call(get_val, (state, false, &escaped, &is_got_val, escontext));
+			if (!is_got_val)
+				return true;
 			if (state->pcur >= state->plen)
 			{
 				state->plen *= 2;
 				state->pairs = (Pairs *) repalloc(state->pairs, sizeof(Pairs) * state->plen);
 			}
 			state->pairs[state->pcur].key = state->word;
-			state->pairs[state->pcur].keylen = hstoreCheckKeyLen(state->cur - state->word);
+			safe_call(hstoreCheckKeyLenSafe, (state->cur - state->word, &state->pairs[state->pcur].keylen, escontext));
 			state->pairs[state->pcur].val = NULL;
 			state->word = NULL;
 			st = WEQ;
@@ -216,11 +235,15 @@ parse_hstore(HSParser *state)
 			}
 			else if (*(state->ptr) == '\0')
 			{
-				elog(ERROR, "Unexpected end of string");
+				ereturn(escontext, false, 
+					(errcode(ERRCODE_INVALID_TEXT_REPRESENTATION),
+					errmsg("Unexpected end of string")));
 			}
 			else if (!isspace((unsigned char) *(state->ptr)))
 			{
-				elog(ERROR, "Syntax error near '%c' at position %d", *(state->ptr), (int32) (state->ptr - state->begin));
+				ereturn(escontext, false, 
+					(errcode(ERRCODE_INVALID_TEXT_REPRESENTATION),
+					errmsg("Syntax error near '%c' at position %d", *(state->ptr), (int32) (state->ptr - state->begin))));
 			}
 		}
 		else if (st == WGT)
@@ -231,19 +254,26 @@ parse_hstore(HSParser *state)
 			}
 			else if (*(state->ptr) == '\0')
 			{
-				elog(ERROR, "Unexpected end of string");
+				ereturn(escontext, false, 
+					(errcode(ERRCODE_INVALID_TEXT_REPRESENTATION),
+					errmsg(ERROR, "Unexpected end of string")));
 			}
 			else
 			{
-				elog(ERROR, "Syntax error near '%c' at position %d", *(state->ptr), (int32) (state->ptr - state->begin));
+				ereturn(escontext, false, 
+					(errcode(ERRCODE_INVALID_TEXT_REPRESENTATION),
+					errmsg("Syntax error near '%c' at position %d", *(state->ptr), (int32) (state->ptr - state->begin))));
 			}
 		}
 		else if (st == WVAL)
 		{
-			if (!get_val(state, true, &escaped))
-				elog(ERROR, "Unexpected end of string");
+			safe_call(get_val, (state, true, &escaped, &is_got_val, escontext));
+			if (!is_got_val)
+				ereturn(escontext, false, 
+					(errcode(ERRCODE_INVALID_TEXT_REPRESENTATION),
+					errmsg("Unexpected end of string")));
 			state->pairs[state->pcur].val = state->word;
-			state->pairs[state->pcur].vallen = hstoreCheckValLen(state->cur - state->word);
+			safe_call(hstoreCheckValLenSafe, (state->cur - state->word, &(state->pairs[state->pcur].vallen), escontext));
 			state->pairs[state->pcur].isnull = false;
 			state->pairs[state->pcur].needfree = true;
 			if (state->cur - state->word == 4 && !escaped)
@@ -264,15 +294,19 @@ parse_hstore(HSParser *state)
 			}
 			else if (*(state->ptr) == '\0')
 			{
-				return;
+				return true;
 			}
 			else if (!isspace((unsigned char) *(state->ptr)))
 			{
-				elog(ERROR, "Syntax error near '%c' at position %d", *(state->ptr), (int32) (state->ptr - state->begin));
+				ereturn(escontext, false, 
+					(errcode(ERRCODE_INVALID_TEXT_REPRESENTATION),
+					errmsg("Syntax error near '%c' at position %d", *(state->ptr), (int32) (state->ptr - state->begin))));
 			}
 		}
 		else
-			elog(ERROR, "Unknown state %d at line %d in file '%s'", st, __LINE__, __FILE__);
+			ereturn(escontext, false, 
+					(errcode(ERRCODE_INVALID_TEXT_REPRESENTATION),
+					errmsg("Unknown state %d at line %d in file '%s'", st, __LINE__, __FILE__)));
 
 		state->ptr++;
 	}
@@ -354,21 +388,37 @@ hstoreUniquePairs(Pairs *a, int32 l, int32 *buflen)
 size_t
 hstoreCheckKeyLen(size_t len)
 {
+	size_t result;
+	(void) hstoreCheckKeyLenSafe(len, &result, NULL);
+	return result;
+}
+bool
+hstoreCheckKeyLenSafe(size_t len, size_t *result, Node *escontext)
+{
 	if (len > HSTORE_MAX_KEY_LEN)
-		ereport(ERROR,
+		ereturn(escontext, false,
 				(errcode(ERRCODE_STRING_DATA_RIGHT_TRUNCATION),
 				 errmsg("string too long for hstore key")));
-	return len;
+	*result = len;
+	return true;
 }
 
 size_t
 hstoreCheckValLen(size_t len)
 {
+	size_t result;
+	(void) hstoreCheckValLenSafe(len, &result, NULL);
+	return result;
+}
+bool
+hstoreCheckValLenSafe(size_t len, size_t *result, Node *escontext)
+{
 	if (len > HSTORE_MAX_VALUE_LEN)
-		ereport(ERROR,
+		ereturn(escontext, false,
 				(errcode(ERRCODE_STRING_DATA_RIGHT_TRUNCATION),
 				 errmsg("string too long for hstore value")));
-	return len;
+	*result = len;
+	return true;
 }
 
 
@@ -412,7 +462,7 @@ hstore_in(PG_FUNCTION_ARGS)
 
 	state.begin = PG_GETARG_CSTRING(0);
 
-	parse_hstore(&state);
+	PG_SAFE_CALL(parse_hstore, (&state, fcinfo->context));
 
 	state.pcur = hstoreUniquePairs(state.pairs, state.pcur, &buflen);
 

contrib/hstore/hstore.h
@@ -6,6 +6,7 @@
 
 #include "fmgr.h"
 #include "utils/array.h"
+#include "nodes/nodes.h"
 
 
 /*
@@ -171,7 +172,9 @@ typedef struct
 extern int	hstoreUniquePairs(Pairs *a, int32 l, int32 *buflen);
 extern HStore *hstorePairs(Pairs *pairs, int32 pcount, int32 buflen);
 
+extern bool hstoreCheckKeyLenSafe(size_t len, size_t *result, Node *escontext);
 extern size_t hstoreCheckKeyLen(size_t len);
+extern bool hstoreCheckValLenSafe(size_t len, size_t *result, Node *escontext);
 extern size_t hstoreCheckValLen(size_t len);
 
 extern int	hstoreFindKey(HStore *hs, int *lowbound, char *key, int keylen);

src/backend/utils/adt/bool.c
@@ -148,7 +148,7 @@ boolin(PG_FUNCTION_ARGS)
 	if (parse_bool_with_len(str, len, &result))
 		PG_RETURN_BOOL(result);
 
-	ereport(ERROR,
+	PG_ERETURN(fcinfo->context,
 			(errcode(ERRCODE_INVALID_TEXT_REPRESENTATION),
 		   errmsg("invalid input syntax for type boolean: \"%s\"", in_str)));
 

src/backend/utils/adt/char.c
@@ -164,7 +164,7 @@ i4tochar(PG_FUNCTION_ARGS)
 	int32		arg1 = PG_GETARG_INT32(0);
 
 	if (arg1 < SCHAR_MIN || arg1 > SCHAR_MAX)
-		ereport(ERROR,
+		PG_ERETURN(fcinfo->context,
 				(errcode(ERRCODE_NUMERIC_VALUE_OUT_OF_RANGE),
 				 errmsg("\"char\" out of range")));
 

src/backend/utils/adt/float.c
@@ -194,7 +194,7 @@ float4in(PG_FUNCTION_ARGS)
 	 * strtod() on different platforms.
 	 */
 	if (*num == '\0')
-		ereturn(fcinfo->context, 0,
+		PG_ERETURN(fcinfo->context,
 				(errcode(ERRCODE_INVALID_TEXT_REPRESENTATION),
 				 errmsg("invalid input syntax for type real: \"%s\"",
 						orig_num)));
@@ -262,13 +262,13 @@ float4in(PG_FUNCTION_ARGS)
 			 * to see if the result is zero or huge.
 			 */
 			if (val == 0.0 || val >= HUGE_VAL || val <= -HUGE_VAL)
-				ereturn(fcinfo->context, 0,
+				PG_ERETURN(fcinfo->context,
 						(errcode(ERRCODE_NUMERIC_VALUE_OUT_OF_RANGE),
 						 errmsg("\"%s\" is out of range for type real",
 								orig_num)));
 		}
 		else
-			ereturn(fcinfo->context, 0,
+			PG_ERETURN(fcinfo->context,
 					(errcode(ERRCODE_INVALID_TEXT_REPRESENTATION),
 					 errmsg("invalid input syntax for type real: \"%s\"",
 							orig_num)));
@@ -292,7 +292,7 @@ float4in(PG_FUNCTION_ARGS)
 
 	/* if there is any junk left at the end of the string, bail out */
 	if (*endptr != '\0')
-		ereturn(fcinfo->context, 0,
+		PG_ERETURN(fcinfo->context,
 				(errcode(ERRCODE_INVALID_TEXT_REPRESENTATION),
 				 errmsg("invalid input syntax for type real: \"%s\"",
 						orig_num)));
@@ -301,7 +301,7 @@ float4in(PG_FUNCTION_ARGS)
 	 * if we get here, we have a legal double, still need to check to see if
 	 * it's a legal float4
 	 */
-	CHECKFLOATVAL((float4) val, isinf(val), val == 0);
+	PG_CHECKFLOATVAL_SAFE((float4) val, isinf(val), val == 0);
 
 	PG_RETURN_FLOAT4((float4) val);
 }
@@ -394,7 +394,7 @@ float8in(PG_FUNCTION_ARGS)
 	 * strtod() on different platforms.
 	 */
 	if (*num == '\0')
-		ereturn(fcinfo->context, 0,
+		PG_ERETURN(fcinfo->context,
 				(errcode(ERRCODE_INVALID_TEXT_REPRESENTATION),
 			 errmsg("invalid input syntax for type double precision: \"%s\"",
 					orig_num)));
@@ -462,13 +462,13 @@ float8in(PG_FUNCTION_ARGS)
 			 * to see if the result is zero or huge.
 			 */
 			if (val == 0.0 || val >= HUGE_VAL || val <= -HUGE_VAL)
-				ereturn(fcinfo->context, 0,
+				PG_ERETURN(fcinfo->context,
 						(errcode(ERRCODE_NUMERIC_VALUE_OUT_OF_RANGE),
 				   errmsg("\"%s\" is out of range for type double precision",
 						  orig_num)));
 		}
 		else
-			ereturn(fcinfo->context, 0,
+			PG_ERETURN(fcinfo->context,
 					(errcode(ERRCODE_INVALID_TEXT_REPRESENTATION),
 			 errmsg("invalid input syntax for type double precision: \"%s\"",
 					orig_num)));
@@ -492,7 +492,7 @@ float8in(PG_FUNCTION_ARGS)
 
 	/* if there is any junk left at the end of the string, bail out */
 	if (*endptr != '\0')
-		ereturn(fcinfo->context, 0,
+		PG_ERETURN(fcinfo->context,
 				(errcode(ERRCODE_INVALID_TEXT_REPRESENTATION),
 			 errmsg("invalid input syntax for type double precision: \"%s\"",
 					orig_num)));
@@ -520,7 +520,7 @@ float8in(PG_FUNCTION_ARGS)
 		 pg_strncasecmp(num, "+Infinity", 9) != 0 && pg_strncasecmp(num, "+Inf", 4) != 0 )
 		literal_inf = false;
 
-	CHECKFLOATVAL((double) val, literal_inf, !(val > 0 || val < 0));
+	PG_CHECKFLOATVAL_SAFE((double) val, literal_inf, !(val > 0 || val < 0));
 
 	PG_RETURN_FLOAT8((double) val);
 }
@@ -751,7 +751,7 @@ float4pl(PG_FUNCTION_ARGS)
 	 * different, e.g. on x86, '1e-45'::float4 == '2e-45'::float4 ==
 	 * 1.4013e-45.
 	 */
-	CHECKFLOATVAL(result, isinf(arg1) || isinf(arg2), true);
+	PG_CHECKFLOATVAL_SAFE(result, isinf(arg1) || isinf(arg2), true);
 	PG_RETURN_FLOAT4(result);
 }
 
@@ -763,7 +763,7 @@ float4mi(PG_FUNCTION_ARGS)
 	float4		result;
 
 	result = arg1 - arg2;
-	CHECKFLOATVAL(result, isinf(arg1) || isinf(arg2), true);
+	PG_CHECKFLOATVAL_SAFE(result, isinf(arg1) || isinf(arg2), true);
 	PG_RETURN_FLOAT4(result);
 }
 
@@ -775,8 +775,8 @@ float4mul(PG_FUNCTION_ARGS)
 	float4		result;
 
 	result = arg1 * arg2;
-	CHECKFLOATVAL(result, isinf(arg1) || isinf(arg2),
-				  arg1 == 0 || arg2 == 0);
+	PG_CHECKFLOATVAL_SAFE(result, isinf(arg1) || isinf(arg2),
+				  		  arg1 == 0 || arg2 == 0);
 	PG_RETURN_FLOAT4(result);
 }
 
@@ -788,13 +788,13 @@ float4div(PG_FUNCTION_ARGS)
 	float4		result;
 
 	if (arg2 == 0.0)
-		ereturn(fcinfo->context, 0,
+		PG_ERETURN(fcinfo->context,
 				(errcode(ERRCODE_DIVISION_BY_ZERO),
 				 errmsg("division by zero")));
 
 	result = arg1 / arg2;
 
-	CHECKFLOATVAL(result, isinf(arg1) || isinf(arg2), arg1 == 0);
+	PG_CHECKFLOATVAL_SAFE(result, isinf(arg1) || isinf(arg2), arg1 == 0);
 	PG_RETURN_FLOAT4(result);
 }
 
@@ -813,7 +813,7 @@ float8pl(PG_FUNCTION_ARGS)
 
 	result = arg1 + arg2;
 
-	CHECKFLOATVAL(result, isinf(arg1) || isinf(arg2), true);
+	PG_CHECKFLOATVAL_SAFE(result, isinf(arg1) || isinf(arg2), true);
 	PG_RETURN_FLOAT8(result);
 }
 
@@ -826,7 +826,7 @@ float8mi(PG_FUNCTION_ARGS)
 
 	result = arg1 - arg2;
 
-	CHECKFLOATVAL(result, isinf(arg1) || isinf(arg2), true);
+	PG_CHECKFLOATVAL_SAFE(result, isinf(arg1) || isinf(arg2), true);
 	PG_RETURN_FLOAT8(result);
 }
 
@@ -839,8 +839,8 @@ float8mul(PG_FUNCTION_ARGS)
 
 	result = arg1 * arg2;
 
-	CHECKFLOATVAL(result, isinf(arg1) || isinf(arg2),
-				  arg1 == 0 || arg2 == 0);
+	PG_CHECKFLOATVAL_SAFE(result, isinf(arg1) || isinf(arg2),
+				  		  arg1 == 0 || arg2 == 0);
 	PG_RETURN_FLOAT8(result);
 }
 
@@ -852,13 +852,13 @@ float8div(PG_FUNCTION_ARGS)
 	float8		result;
 
 	if (arg2 == 0.0)
-		ereturn(fcinfo->context, 0,
+		PG_ERETURN(fcinfo->context,
 				(errcode(ERRCODE_DIVISION_BY_ZERO),
 				 errmsg("division by zero")));
 
 	result = arg1 / arg2;
 
-	CHECKFLOATVAL(result, isinf(arg1) || isinf(arg2), arg1 == 0);
+	PG_CHECKFLOATVAL_SAFE(result, isinf(arg1) || isinf(arg2), arg1 == 0);
 	PG_RETURN_FLOAT8(result);
 }
 
@@ -1144,7 +1144,7 @@ dtof(PG_FUNCTION_ARGS)
 {
 	float8		num = PG_GETARG_FLOAT8(0);
 
-	CHECKFLOATVAL((float4) num, isinf(num), num == 0);
+	PG_CHECKFLOATVAL_SAFE((float4) num, isinf(num), num == 0);
 
 	PG_RETURN_FLOAT4((float4) num);
 }
@@ -1167,7 +1167,7 @@ dtoi4(PG_FUNCTION_ARGS)
 
 	/* Range check */
 	if (isnan(num) || !FLOAT8_FITS_IN_INT32(num))
-		ereturn(fcinfo->context, 0,
+		PG_ERETURN(fcinfo->context,
 				(errcode(ERRCODE_NUMERIC_VALUE_OUT_OF_RANGE),
 				 errmsg("integer out of range")));
 
@@ -1192,7 +1192,7 @@ dtoi2(PG_FUNCTION_ARGS)
 
 	/* Range check */
 	if (isnan(num) || !FLOAT8_FITS_IN_INT16(num))
-		ereturn(fcinfo->context, 0,
+		PG_ERETURN(fcinfo->context,
 				(errcode(ERRCODE_NUMERIC_VALUE_OUT_OF_RANGE),
 				 errmsg("smallint out of range")));
 
@@ -1241,7 +1241,7 @@ ftoi4(PG_FUNCTION_ARGS)
 
 	/* Range check */
 	if (isnan(num) || !FLOAT4_FITS_IN_INT32(num))
-		ereturn(fcinfo->context, 0,
+		PG_ERETURN(fcinfo->context,
 				(errcode(ERRCODE_NUMERIC_VALUE_OUT_OF_RANGE),
 				 errmsg("integer out of range")));
 
@@ -1266,7 +1266,7 @@ ftoi2(PG_FUNCTION_ARGS)
 
 	/* Range check */
 	if (isnan(num) || !FLOAT4_FITS_IN_INT16(num))
-		ereturn(fcinfo->context, 0,
+		PG_ERETURN(fcinfo->context,
 				(errcode(ERRCODE_NUMERIC_VALUE_OUT_OF_RANGE),
 				 errmsg("smallint out of range")));
 
@@ -2647,7 +2647,7 @@ float48pl(PG_FUNCTION_ARGS)
 	float8		result;
 
 	result = arg1 + arg2;
-	CHECKFLOATVAL(result, isinf(arg1) || isinf(arg2), true);
+	PG_CHECKFLOATVAL_SAFE(result, isinf(arg1) || isinf(arg2), true);
 	PG_RETURN_FLOAT8(result);
 }
 
@@ -2659,7 +2659,7 @@ float48mi(PG_FUNCTION_ARGS)
 	float8		result;
 
 	result = arg1 - arg2;
-	CHECKFLOATVAL(result, isinf(arg1) || isinf(arg2), true);
+	PG_CHECKFLOATVAL_SAFE(result, isinf(arg1) || isinf(arg2), true);
 	PG_RETURN_FLOAT8(result);
 }
 
@@ -2671,7 +2671,7 @@ float48mul(PG_FUNCTION_ARGS)
 	float8		result;
 
 	result = arg1 * arg2;
-	CHECKFLOATVAL(result, isinf(arg1) || isinf(arg2),
+	PG_CHECKFLOATVAL_SAFE(result, isinf(arg1) || isinf(arg2),
 				  arg1 == 0 || arg2 == 0);
 	PG_RETURN_FLOAT8(result);
 }
@@ -2684,12 +2684,12 @@ float48div(PG_FUNCTION_ARGS)
 	float8		result;
 
 	if (arg2 == 0.0)
-		ereturn(fcinfo->context, 0,
+		PG_ERETURN(fcinfo->context,
 				(errcode(ERRCODE_DIVISION_BY_ZERO),
 				 errmsg("division by zero")));
 
 	result = arg1 / arg2;
-	CHECKFLOATVAL(result, isinf(arg1) || isinf(arg2), arg1 == 0);
+	PG_CHECKFLOATVAL_SAFE(result, isinf(arg1) || isinf(arg2), arg1 == 0);
 	PG_RETURN_FLOAT8(result);
 }
 
@@ -2708,7 +2708,7 @@ float84pl(PG_FUNCTION_ARGS)
 
 	result = arg1 + arg2;
 
-	CHECKFLOATVAL(result, isinf(arg1) || isinf(arg2), true);
+	PG_CHECKFLOATVAL_SAFE(result, isinf(arg1) || isinf(arg2), true);
 	PG_RETURN_FLOAT8(result);
 }
 
@@ -2721,7 +2721,7 @@ float84mi(PG_FUNCTION_ARGS)
 
 	result = arg1 - arg2;
 
-	CHECKFLOATVAL(result, isinf(arg1) || isinf(arg2), true);
+	PG_CHECKFLOATVAL_SAFE(result, isinf(arg1) || isinf(arg2), true);
 	PG_RETURN_FLOAT8(result);
 }
 
@@ -2734,8 +2734,8 @@ float84mul(PG_FUNCTION_ARGS)
 
 	result = arg1 * arg2;
 
-	CHECKFLOATVAL(result, isinf(arg1) || isinf(arg2),
-				  arg1 == 0 || arg2 == 0);
+	PG_CHECKFLOATVAL_SAFE(result, isinf(arg1) || isinf(arg2),
+						  arg1 == 0 || arg2 == 0);
 	PG_RETURN_FLOAT8(result);
 }
 
@@ -2747,13 +2747,13 @@ float84div(PG_FUNCTION_ARGS)
 	float8		result;
 
 	if (arg2 == 0.0)
-		ereturn(fcinfo->context, 0,
+		PG_ERETURN(fcinfo->context,
 				(errcode(ERRCODE_DIVISION_BY_ZERO),
 				 errmsg("division by zero")));
 
 	result = arg1 / arg2;
 
-	CHECKFLOATVAL(result, isinf(arg1) || isinf(arg2), arg1 == 0);
+	PG_CHECKFLOATVAL_SAFE(result, isinf(arg1) || isinf(arg2), arg1 == 0);
 	PG_RETURN_FLOAT8(result);
 }
 

commit 4b00e5f1ce85e7db8e1c7fd6992f37e9fd19e3b1

Fix fat fingering in hstore
Broken since d31f3622501

contrib/hstore/hstore_io.c
@@ -256,7 +256,7 @@ parse_hstore(HSParser *state, Node *escontext)
 			{
 				ereturn(escontext, false, 
 					(errcode(ERRCODE_INVALID_TEXT_REPRESENTATION),
-					errmsg(ERROR, "Unexpected end of string")));
+					errmsg("Unexpected end of string")));
 			}
 			else
 			{