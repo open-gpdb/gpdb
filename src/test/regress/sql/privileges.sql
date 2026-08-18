postgres/postgres

Check for USAGE privilege on the composite type in ALTER TABLE OF.
This omission allowed roles without USAGE on a type to create
tables that depend on it, which could prevent the owner from
changing the type later.

Reported-by: Nathan Bossart <nathandbossart@gmail.com>
Author: Nathan Bossart <nathandbossart@gmail.com>
Reviewed-by: Robert Haas <robertmhaas@gmail.com>
Security: CVE-2026-6470
Backpatch-through: 14

src/test/regress/sql/privileges.sql
@@ -941,6 +941,8 @@ CREATE OPERATOR !+! (PROCEDURE = int4pl, LEFTARG = priv_testdomain1, RIGHTARG = 
 
 CREATE TABLE test5a (a int, b priv_testdomain1);
 CREATE TABLE test6a OF priv_testtype1;
+CREATE TABLE test6a2 (a int, b text);
+ALTER TABLE test6a2 OF priv_testtype1;
 CREATE TABLE test10a (a int[], b priv_testtype1[]);
 
 CREATE TABLE test9a (a int, b int);
@@ -989,6 +991,8 @@ CREATE OPERATOR !! (PROCEDURE = priv_testfunc5b, RIGHTARG = priv_testdomain1);
 
 CREATE TABLE test5b (a int, b priv_testdomain1);
 CREATE TABLE test6b OF priv_testtype1;
+CREATE TABLE test6b2 (a int, b text);
+ALTER TABLE test6b2 OF priv_testtype1;
 CREATE TABLE test10b (a int[], b priv_testtype1[]);
 
 CREATE TABLE test9b (a int, b int);
@@ -1036,6 +1040,7 @@ DROP FUNCTION priv_testfunc6b(b int);
 DROP FUNCTION priv_testfunc7b(a int);
 DROP TABLE test5b;
 DROP TABLE test6b;
+DROP TABLE test6b2;
 DROP TABLE test9b;
 DROP TABLE test10b;
 DROP TYPE test7b;

open-gpdb/gpdb

src/test/regress/sql/privileges.sql
@@ -508,6 +508,8 @@ CREATE OPERATOR !+! (PROCEDURE = int4pl, LEFTARG = testdomain1, RIGHTARG = testd
 
 CREATE TABLE test5a (a int, b testdomain1);
 CREATE TABLE test6a OF testtype1;
+CREATE TABLE test6a2 (a int, b text);
+ALTER TABLE test6a2 OF testtype1;
 CREATE TABLE test10a (a int[], b testtype1[]);
 
 CREATE TABLE test9a (a int, b int);
@@ -543,6 +545,8 @@ CREATE OPERATOR !! (PROCEDURE = testfunc5b, RIGHTARG = testdomain1);
 
 CREATE TABLE test5b (a int, b testdomain1);
 CREATE TABLE test6b OF testtype1;
+CREATE TABLE test6b2 (a int, b text);
+ALTER TABLE test6b2 OF testtype1;
 CREATE TABLE test10b (a int[], b testtype1[]);
 
 CREATE TABLE test9b (a int, b int);
@@ -567,6 +571,7 @@ DROP FUNCTION testfunc5b(a testdomain1);
 DROP FUNCTION testfunc6b(b int);
 DROP TABLE test5b;
 DROP TABLE test6b;
+DROP TABLE test6b2;
 DROP TABLE test9b;
 DROP TABLE test10b;
 DROP TYPE test7b;